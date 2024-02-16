bec535f37b9ac7a22628f930d8e823db98cb371f
Bansari Thummar <bansarithummar@gmail.com>
2024-02-16 16:02:27 -0600

    Update twoSum.go

--- twoSum.go
+++ twoSum.go
@@ -1,1 +1,21 @@
-1
+1. Two Sum
+
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	for i, num := range nums {
+		complement := target - num
+		if j, ok := numMap[complement]; ok {
+			return []int{j, i}
+		}
+		numMap[num] = i
+	}
+	return nil
+}
+
+
