fba3b0c6cc070150c387cd179eff209600197e95
Bansari Thummar <bansarithummar@gmail.com>
2024-02-20 11:11:00 -0600

    Update rearrangearray.go

--- rearrangearray.go
+++ rearrangearray.go
@@ -1,33 +1,19 @@
 2149. Rearrange Array Elements by Sign
 
-package main
+func rearrangeArray(nums []int) []int {
+    pi := 0
+    ni := 1
+    res := make([]int, len(nums))
 
-import "fmt"
+    for _, n := range nums {
+        if n > 0 {
+            res[pi] = n
+            pi += 2
+        } else {
+            res[ni] = n
+            ni += 2
+        }
+    }
 
-func rearrange(nums []int) []int {
-	// Separate positive and negative integers
-	var positives []int
-	var negatives []int
-	for _, num := range nums {
-		if num > 0 {
-			positives = append(positives, num)
-		} else {
-			negatives = append(negatives, num)
-		}
-	}
-
-	// Rearrange the elements following the given conditions
-	result := make([]int, 0, len(nums))
-	i, j := 0, 0
-	for i < len(positives) && j < len(negatives) {
-		result = append(result, positives[i], negatives[j])
-		i++
-		j++
-	}
-
-	// Append the remaining elements from the non-empty array
-	result = append(result, positives[i:]...)
-	result = append(result, negatives[j:]...)
-
-	return result
+    return res
 }
