fa86889ac50282f6d2a00cc6f5775e9b668497a1
Bansari Thummar <bansarithummar@gmail.com>
2024-02-19 21:30:00 -0600

    Update isAnagram.go

--- isAnagram.go
+++ isAnagram.go
@@ -1,1 +1,21 @@
 242. Valid Anagram
+
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+    countS := make(map[rune]int)
+    countT := make(map[rune]int)
+    for _, c := range s {
+        countS[c]++
+    }
+    for _, c := range t {
+        countT[c]++
+    }
+    for c, count := range countS {
+        if count != countT[c] {
+            return false
+        }
+    }
+    return true
+}
