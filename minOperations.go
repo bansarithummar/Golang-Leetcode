5d0c32a1d45f821ac9b0a25d1d783c3f56c44dd3
Bansari Thummar <bansarithummar@gmail.com>
2024-02-20 11:09:20 -0600

    Update minOperations.go

--- minOperations.go
+++ minOperations.go
@@ -1,49 +1,36 @@
 1658. Minimum Operations to Reduce X to Zero
 
-package main
-
-import (
-	"fmt"
-)
-
 func minOperations(nums []int, x int) int {
-	target := -x
-	for _, num := range nums {
-		target += num
-	}
-	
-	if target == 0 {
-		return len(nums)
-	}
-	
-	left, right := 0, 0
-	sum := 0
-	minOps := -1
-	
-	for right < len(nums) {
-		sum += nums[right]
-		right++
-		
-		for sum > target && left < right {
-			sum -= nums[left]
-			left++
-		}
-		
-		if sum == target {
-			if minOps == -1 || len(nums)-right+left < minOps {
-				minOps = len(nums) - right + left
-			}
-		}
-	}
-	
-	if minOps == -1 {
-		return -1
-	}
-	
-	return minOps
+    n := len(nums)
+    target := 0
+    for _, num := range nums {
+        target += num
+    }
+    target -= x
+    
+    prefixSum := make(map[int]int)
+    prefixSum[0] = -1
+    currSum := 0
+    maxLength := -1
+
+    for i, num := range nums {
+        currSum += num
+        if idx, exists := prefixSum[currSum - target]; exists {
+            maxLength = max(maxLength, i - idx)
+        }
+        prefixSum[currSum] = i
+    }
+
+    if maxLength == -1 {
+        return -1
+    } else {
+        return n - maxLength
+    }
 }
 
-func main() {
-	fmt.Println(minOperations([]int{1,1,4,2,3}, 5)) // Output: 2
-	fmt.Println(minOperations([]int{5,6,7,8,9}, 4)) // Output: -1
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
 }
