2f8af55ac1761aca1d9078288cc40d3867a9ec05
Bansari Thummar <bansarithummar@gmail.com>
2024-02-19 21:27:29 -0600

    Update containsDuplicate.go

--- containsDuplicate.go
+++ containsDuplicate.go
@@ -1,1 +1,13 @@
+217. Contains Duplicate
+
+func containsDuplicate(nums []int) bool {
+    hashset := make(map[int]bool)
+    for _, n := range nums {
+        if hashset[n] {
+            return true
+        }
+        hashset[n] = true
+    }
+    return false
+}
 
