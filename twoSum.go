11a7a62771e8dda770031420136619b05f7e2f30
Bansari Thummar <bansarithummar@gmail.com>
2024-02-20 11:14:37 -0600

    Update twoSum.go

--- twoSum.go
+++ twoSum.go
@@ -2,20 +2,20 @@
 
 package main
 
-import (
-	"fmt"
-)
-
 func twoSum(nums []int, target int) []int {
-	numMap := make(map[int]int)
-	for i, num := range nums {
-		complement := target - num
-		if j, ok := numMap[complement]; ok {
-			return []int{j, i}
+	prevMap := make(map[int]int)
+	for i, n := range nums {
+		diff := target - n
+		if index, found := prevMap[diff]; found {
+			return []int{index, i}
 		}
-		numMap[num] = i
+		prevMap[n] = i
 	}
 	return nil
 }
 
-
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(nums, target)) // Output: [0, 1]
+}
