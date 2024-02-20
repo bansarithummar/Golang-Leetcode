da685328d2e38ab0b6bc728e19a42a568adf41cc
Bansari Thummar <bansarithummar@gmail.com>
2024-02-20 11:02:24 -0600

    Update addTwoNumbers.go

--- addTwoNumbers.go
+++ addTwoNumbers.go
@@ -1,65 +1,25 @@
 2. Add Two Numbers
 
-package main
-
-import (
-	"fmt"
-)
-
-// Definition for singly-linked list.
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	// Initialize dummy head node for the result list
-	dummyHead := &ListNode{}
-	current := dummyHead
-	carry := 0
-
-	// Iterate through both linked lists until both are nil and carry is zero
-	for l1 != nil || l2 != nil || carry > 0 {
-		// Calculate the sum of the current digits and carry
-		sum := carry
-		if l1 != nil {
-			sum += l1.Val
-			l1 = l1.Next
-		}
-		if l2 != nil {
-			sum += l2.Val
-			l2 = l2.Next
-		}
-
-		// Update carry and create a new node with the sum digit
-		carry = sum / 10
-		current.Next = &ListNode{Val: sum % 10}
-		current = current.Next
-	}
-
-	// Return the next node after the dummy head, which contains the result
-	return dummyHead.Next
+    dummy := &ListNode{}
+    cur := dummy
+    carry := 0
+    for l1 != nil || l2 != nil || carry != 0 {
+        v1 := 0
+        if l1 != nil {
+            v1 = l1.Val
+            l1 = l1.Next
+        }
+        v2 := 0
+        if l2 != nil {
+            v2 = l2.Val
+            l2 = l2.Next
+        }
+        sum := v1 + v2 + carry
+        carry = sum / 10
+        val := sum % 10
+        cur.Next = &ListNode{Val: val}
+        cur = cur.Next
+    }
+    return dummy.Next
 }
-
-// Helper function to create a linked list from an array of integers
-func createList(nums []int) *ListNode {
-	dummyHead := &ListNode{}
-	current := dummyHead
-	for _, num := range nums {
-		current.Next = &ListNode{Val: num}
-		current = current.Next
-	}
-	return dummyHead.Next
-}
-
-// Helper function to print the linked list
-func printList(head *ListNode) {
-	for head != nil {
-		fmt.Printf("%d -> ", head.Val)
-		head = head.Next
-	}
-	fmt.Println("nil")
-}
-
-
-
