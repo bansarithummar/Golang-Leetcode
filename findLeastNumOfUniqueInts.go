ade0ce95111e53a9f31dbb28dccc062e53fb854c
Bansari Thummar <bansarithummar@gmail.com>
2024-02-20 11:06:17 -0600

    Update findLeastNumOfUniqueInts.go

--- findLeastNumOfUniqueInts.go
+++ findLeastNumOfUniqueInts.go
@@ -1,44 +1,36 @@
 1481. Least Number of Unique Integers after K Removals
 
-package main
-
 import (
-	"fmt"
-	"sort"
+    "sort"
 )
 
 func findLeastNumOfUniqueInts(arr []int, k int) int {
-	// Count the frequency of each integer
-	freqMap := make(map[int]int)
-	for _, num := range arr {
-		freqMap[num]++
-	}
-	
-	// Store the frequencies in a slice
-	frequencies := make([]int, 0, len(freqMap))
-	for _, freq := range freqMap {
-		frequencies = append(frequencies, freq)
-	}
-	
-	// Sort the frequencies in ascending order
-	sort.Ints(frequencies)
-	
-	// Remove the smallest frequency elements until k elements are removed
-	numUnique := len(frequencies)
-	for _, freq := range frequencies {
-		if k >= freq {
-			k -= freq
-			numUnique--
-		} else {
-			break
-		}
-	}
-	
-	return numUnique
-}
+    freqMap := make(map[int]int)
+    for _, num := range arr {
+        freqMap[num]++
+    }
 
-func main() {
-	fmt.Println(findLeastNumOfUniqueInts([]int{5,5,4}, 1))       // Output: 1
-	fmt.Println(findLeastNumOfUniqueInts([]int{4,3,1,1,3,3,2}, 3)) // Output: 2
-}
+    // Convert the frequency map to a slice of pairs (num, freq)
+    sortedFreq := make([][2]int, 0, len(freqMap))
+    for num, freq := range freqMap {
+        sortedFreq = append(sortedFreq, [2]int{num, freq})
+    }
 
+    // Sort the slice based on frequency
+    sort.Slice(sortedFreq, func(i, j int) bool {
+        return sortedFreq[i][1] < sortedFreq[j][1]
+    })
+
+    // Remove elements based on k until k becomes 0 or there are no more elements to remove
+    for _, pair := range sortedFreq {
+        if k >= pair[1] {
+            k -= pair[1]
+            delete(freqMap, pair[0])
+        } else {
+            break
+        }
+    }
+
+    // The remaining elements in freqMap represent the least number of unique integers
+    return len(freqMap)
+}
