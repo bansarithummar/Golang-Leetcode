8165b95e6d9a1ebaf96b54477e541ddfea8a45c8
Bansari Thummar <bansarithummar@gmail.com>
2024-02-25 20:09:35 -0600

    Update isPalindrome.go

--- isPalindrome.go
+++ isPalindrome.go
@@ -1,1 +1,36 @@
+125. Valid Palindrome
+
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func isPalindrome(s string) bool {
+	l, r := 0, len(s)-1
+	for l < r {
+		for l < r && !isAlphaNum(rune(s[l])) {
+			l++
+		}
+		for r > l && !isAlphaNum(rune(s[r])) {
+			r--
+		}
+		if unicode.ToLower(rune(s[l])) != unicode.ToLower(rune(s[r])) {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func isAlphaNum(c rune) bool {
+	return unicode.IsLetter(c) || unicode.IsDigit(c)
+}
+
+func main() {
+	s := "A man, a plan, a canal: Panama"
+	fmt.Println(isPalindrome(s)) // Output: true
+}
 
