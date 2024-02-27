7cf21a5ffed4aca1dc9227f342a10cce4f5c50bb
Bansari Thummar <bansarithummar@gmail.com>
2024-02-27 14:25:17 -0600

    Update threeSum.go

--- threeSum.go
+++ threeSum.go
@@ -1,1 +1,32 @@
+15. 3Sum
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	var res [][]int
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		l, r := i+1, len(nums)-1
+		for l < r {
+			threeSum := nums[i] + nums[l] + nums[r]
+			if threeSum > 0 {
+				r--
+			} else if threeSum < 0 {
+				l++
+			} else {
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+				l++
+				for nums[l] == nums[l-1] && l < r {
+					l++
+				}
+			}
+		}
+	}
+	return res
+}
 
