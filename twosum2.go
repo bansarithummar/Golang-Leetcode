fb73c8a1da0be1f08c6aad261ff94f5e331005e7
Bansari Thummar <bansarithummar@gmail.com>
2024-02-26 20:06:09 -0600

    Update twosum2.go

--- twosum2.go
+++ twosum2.go
@@ -1,1 +1,28 @@
+167. Two Sum II - Input Array Is Sorted
+
+package main
+
+import "fmt"
+
+func twoSum(numbers []int, target int) []int {
+	l, r := 0, len(numbers)-1
+
+	for l < r {
+		curSum := numbers[l] + numbers[r]
+		if curSum > target {
+			r--
+		} else if curSum < target {
+			l++
+		} else {
+			return []int{l + 1, r + 1}
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	numbers := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(numbers, target)) // Output: [1 2]
+}
 
