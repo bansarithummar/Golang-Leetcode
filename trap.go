f58128629bcc88f58a70ddcc5d4ccbd8e72ed4fe
Bansari Thummar <bansarithummar@gmail.com>
2024-02-28 16:11:38 -0600

    Update trap.go

--- trap.go
+++ trap.go
@@ -1,1 +1,42 @@
+42. Trapping Rain Water
+
+package main
+
+import "fmt"
+
+func trap(height []int) int {
+	if len(height) == 0 {
+		return 0
+	}
+
+	left, right := 0, len(height)-1
+	leftMax, rightMax := height[left], height[right]
+	res := 0
+
+	for left < right {
+		if leftMax < rightMax {
+			left++
+			leftMax = max(leftMax, height[left])
+			res += leftMax - height[left]
+		} else {
+			right--
+			rightMax = max(rightMax, height[right])
+			res += rightMax - height[right]
+		}
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	fmt.Println(trap(height)) // Output: 6
+}
 
