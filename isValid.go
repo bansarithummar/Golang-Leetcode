ea836dcab9fe434c18a6da8079ac3fa0de99f7ce
Bansari Thummar <bansarithummar@gmail.com>
2024-02-29 17:26:45 -0600

    Update isValid.go

--- isValid.go
+++ isValid.go
@@ -1,1 +1,25 @@
+20. Valid Parentheses
+
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+	closeToOpen := map[rune]rune{')': '(', '}': '{', ']': '['}
+
+	for _, c := range s {
+		if _, ok := closeToOpen[c]; ok {
+			if len(stack) > 0 && stack[len(stack)-1] == closeToOpen[c] {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		} else {
+			stack = append(stack, c)
+		}
+	}
+
+	return len(stack) == 0
+}
 
