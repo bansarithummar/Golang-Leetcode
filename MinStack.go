826c8e3e1aa1ab29d47c503b07804eadf0dd881c
Bansari Thummar <bansarithummar@gmail.com>
2024-02-29 17:28:39 -0600

    Update MinStack.go

--- MinStack.go
+++ MinStack.go
@@ -1,1 +1,48 @@
+155. Min Stack
+
+package main
+
+import "math"
+
+type MinStack struct {
+    stack    []int
+    minStack []int
+}
+
+func Constructor() MinStack {
+    return MinStack{
+        stack:    make([]int, 0),
+        minStack: make([]int, 0),
+    }
+}
+
+func (s *MinStack) Push(val int) {
+    s.stack = append(s.stack, val)
+    minVal := val
+    if len(s.minStack) > 0 && s.minStack[len(s.minStack)-1] < val {
+        minVal = s.minStack[len(s.minStack)-1]
+    }
+    s.minStack = append(s.minStack, minVal)
+}
+
+func (s *MinStack) Pop() {
+    if len(s.stack) > 0 {
+        s.stack = s.stack[:len(s.stack)-1]
+        s.minStack = s.minStack[:len(s.minStack)-1]
+    }
+}
+
+func (s *MinStack) Top() int {
+    if len(s.stack) > 0 {
+        return s.stack[len(s.stack)-1]
+    }
+    return -1
+}
+
+func (s *MinStack) GetMin() int {
+    if len(s.minStack) > 0 {
+        return s.minStack[len(s.minStack)-1]
+    }
+    return math.MaxInt64
+}
 
