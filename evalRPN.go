316898a9f9a438acfa4ba2a86851706f63e805d4
Bansari Thummar <bansarithummar@gmail.com>
2024-03-03 20:22:05 -0600

    Update evalRPN.go

--- evalRPN.go
+++ evalRPN.go
@@ -1,1 +1,34 @@
+150. Evaluate Reverse Polish Notation
+
+package main
+
+import "strconv"
+
+func evalRPN(tokens []string) int {
+	stack := []int{}
+	for _, c := range tokens {
+		switch c {
+		case "+":
+			b, a := stack[len(stack)-2], stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, a+b)
+		case "-":
+			b, a := stack[len(stack)-2], stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, b-a)
+		case "*":
+			b, a := stack[len(stack)-2], stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, a*b)
+		case "/":
+			b, a := stack[len(stack)-2], stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, b/a)
+		default:
+			num, _ := strconv.Atoi(c)
+			stack = append(stack, num)
+		}
+	}
+	return stack[0]
+}
 
