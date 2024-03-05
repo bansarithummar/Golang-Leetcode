4a5cfddb0ce19efdce4869e156427c27d19e26e0
Bansari Thummar <bansarithummar@gmail.com>
2024-03-05 15:11:49 -0600

    Update carFleet.go

--- carFleet.go
+++ carFleet.go
@@ -1,1 +1,35 @@
-Car Fleet
+853. Car Fleet
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func carFleet(target int, position []int, speed []int) int {
+	type pair struct {
+		position int
+		speed    int
+	}
+	var pairs []pair
+	for i := 0; i < len(position); i++ {
+		pairs = append(pairs, pair{position[i], speed[i]})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].position > pairs[j].position
+	})
+
+	var stack []float64
+	for _, p := range pairs {
+		time := float64(target-p.position) / float64(p.speed)
+		stack = append(stack, time)
+		if len(stack) >= 2 && stack[len(stack)-1] <= stack[len(stack)-2] {
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack)
+}
+
