8c2bf6922c8b1d281ac5faeea1d0ee91a2e17b14
Bansari Thummar <bansarithummar@gmail.com>
2024-03-04 21:35:15 -0600

    Update dailyTemperatures.go

--- dailyTemperatures.go
+++ dailyTemperatures.go
@@ -1,1 +1,22 @@
+739. Daily Temperatures
+
+package main
+
+import "fmt"
+
+func dailyTemperatures(temperatures []int) []int {
+	res := make([]int, len(temperatures))
+	var stack [][]int
+
+	for i, t := range temperatures {
+		for len(stack) > 0 && t > stack[len(stack)-1][0] {
+			stackT, stackInd := stack[len(stack)-1][0], stack[len(stack)-1][1]
+			res[stackInd] = i - stackInd
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, []int{t, i})
+	}
+
+	return res
+}
 
