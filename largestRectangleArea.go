3cd8b01d3a1e68ad6e70e2aea01b72aff80d527b
Bansari Thummar <bansarithummar@gmail.com>
2024-03-05 15:13:59 -0600

    Update largestRectangleArea.go

--- largestRectangleArea.go
+++ largestRectangleArea.go
@@ -1,1 +1,35 @@
+84. Largest Rectangle in Histogram
 
+package main
+
+import "fmt"
+
+func largestRectangleArea(heights []int) int {
+	maxArea := 0
+	var stack [][]int
+
+	for i, h := range heights {
+		start := i
+		for len(stack) > 0 && stack[len(stack)-1][1] > h {
+			index, height := stack[len(stack)-1][0], stack[len(stack)-1][1]
+			stack = stack[:len(stack)-1]
+			maxArea = max(maxArea, height*(i-index))
+			start = index
+		}
+		stack = append(stack, []int{start, h})
+	}
+
+	for _, pair := range stack {
+		index, height := pair[0], pair[1]
+		maxArea = max(maxArea, height*(len(heights)-index))
+	}
+
+	return maxArea
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
