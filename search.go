683b319fac57f477d700ba71f886805d0f6f8127
Bansari Thummar <bansarithummar@gmail.com>
2024-03-06 20:44:56 -0600

    Update search.go

--- search.go
+++ search.go
@@ -1,1 +1,22 @@
+704. Binary Search
 
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		m := (l + r) / 2
+		if nums[m] > target {
+			r = m - 1
+		} else if nums[m] < target {
+			l = m + 1
+		} else {
+			return m
+		}
+	}
+
+	return -1
+}
