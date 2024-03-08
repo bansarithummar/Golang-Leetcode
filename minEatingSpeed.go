f5f0daac36caf3dc8aae8aa0cdd62763772605d1
Bansari Thummar <bansarithummar@gmail.com>
2024-03-08 14:51:43 -0600

    Update minEatingSpeed.go

--- minEatingSpeed.go
+++ minEatingSpeed.go
@@ -1,1 +1,46 @@
+875. Koko Eating Bananas
 
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func minEatingSpeed(piles []int, h int) int {
+	left, right := 1, max(piles)
+	res := right
+
+	for left <= right {
+		k := (left + right) / 2
+		hours := 0
+		for _, p := range piles {
+			hours += int(math.Ceil(float64(p) / float64(k)))
+		}
+
+		if hours <= h {
+			res = min(res, k)
+			right = k - 1
+		} else {
+			left = k + 1
+		}
+	}
+	return res
+}
+
+func max(arr []int) int {
+	max := arr[0]
+	for _, num := range arr {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
