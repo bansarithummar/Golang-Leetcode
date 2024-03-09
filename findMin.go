802de0175c993e1a2386d77550db7fa5de66d0e6
Bansari Thummar <bansarithummar@gmail.com>
2024-03-09 16:14:29 -0600

    Update findMin.go

--- findMin.go
+++ findMin.go
@@ -1,1 +1,34 @@
+153. Find Minimum in Rotated Sorted Array
 
+package main
+
+import (
+	"fmt"
+)
+
+func findMin(nums []int) int {
+	res := nums[0]
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		if nums[left] < nums[right] {
+			res = min(res, nums[left])
+			break
+		}
+		mid := (left + right) / 2
+		res = min(res, nums[mid])
+		if nums[mid] >= nums[left] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
