b49a31be292552ff5232fab8a91c104354f3106b
Bansari Thummar <bansarithummar@gmail.com>
2024-03-14 21:34:28 -0500

    Create maxProfit.go

---
+++ maxProfit.go
@@ -0,0 +1,30 @@
+121. Best Time to Buy and Sell Stock
+
+package main
+
+import (
+	"fmt"
+)
+
+func maxProfit(prices []int) int {
+	l, r := 0, 1
+	maxP := 0
+	for r < len(prices) {
+		if prices[l] < prices[r] {
+			profit := prices[r] - prices[l]
+			maxP = max(maxP, profit)
+		} else {
+			l = r
+		}
+		r++
+	}
+	return maxP
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
