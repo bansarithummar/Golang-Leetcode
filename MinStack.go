fdab9e8d6d4389bd59d323f3cd96c38da188df5c
Bansari Thummar <bansarithummar@gmail.com>
2024-03-19 10:57:45 -0500

    Update MinStack.go

--- MinStack.go
+++ MinStack.go
@@ -1,44 +1,52 @@
 155. Min Stack
 
+package main
+
+import "fmt"
+
 type MinStack struct {
-    stack    []int
-    minStack []int
+    stack    []int // Stack to store all elements
+    minStack []int // Stack to store the minimum elements
 }
 
 func Constructor() MinStack {
     return MinStack{
-        stack:    make([]int, 0),
-        minStack: make([]int, 0),
+        stack:    []int{},
+        minStack: []int{},
     }
 }
 
-func (s *MinStack) Push(val int) {
-    s.stack = append(s.stack, val)
-    minVal := val
-    if len(s.minStack) > 0 && s.minStack[len(s.minStack)-1] < val {
-        minVal = s.minStack[len(s.minStack)-1]
-    }
-    s.minStack = append(s.minStack, minVal)
-}
-
-func (s *MinStack) Pop() {
-    if len(s.stack) > 0 {
-        s.stack = s.stack[:len(s.stack)-1]
-        s.minStack = s.minStack[:len(s.minStack)-1]
+func (this *MinStack) Push(val int) {
+    this.stack = append(this.stack, val)
+    if len(this.minStack) == 0 || val <= this.minStack[len(this.minStack)-1] {
+        this.minStack = append(this.minStack, val)
     }
 }
 
-func (s *MinStack) Top() int {
-    if len(s.stack) > 0 {
-        return s.stack[len(s.stack)-1]
+func (this *MinStack) Pop() {
+    if len(this.stack) > 0 {
+        topElement := this.stack[len(this.stack)-1]
+        this.stack = this.stack[:len(this.stack)-1]
+        if topElement == this.minStack[len(this.minStack)-1] {
+            this.minStack = this.minStack[:len(this.minStack)-1]
+        }
     }
-    return -1
 }
 
-func (s *MinStack) GetMin() int {
-    if len(s.minStack) > 0 {
-        return s.minStack[len(s.minStack)-1]
+func (this *MinStack) Top() int {
+    if len(this.stack) > 0 {
+        return this.stack[len(this.stack)-1]
     }
-    return math.MaxInt64
+    return -1 // Or handle error appropriately
 }
+
+func (this *MinStack) GetMin() int {
+    if len(this.minStack) > 0 {
+        return this.minStack[len(this.minStack)-1]
+    }
+    return -1 // Or handle error appropriately
+}
+
+// Ensure to integrate the testing part in your own main function, or run this snippet in isolation.
+
 
