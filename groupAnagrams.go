e20ab399ed94fdec93b520508d1cf78e043ba916
Bansari Thummar <bansarithummar@gmail.com>
2024-03-20 21:29:53 -0500

    Update groupAnagrams.go

--- groupAnagrams.go
+++ groupAnagrams.go
@@ -1,40 +1,22 @@
 49.Group Anagrams
 
-package main
-
-import (
-    "fmt"
-    "sort"
-    "strings"
-)
-
+func sortString(w string) string {
+    s := strings.Split(w, "")
+    sort.Strings(s)
+    return strings.Join(s, "")
+}
 func groupAnagrams(strs []string) [][]string {
-    // Create a map to store the groups of anagrams
     groups := make(map[string][]string)
-
-    // Iterate over each string in strs
     for _, str := range strs {
-        // Convert the string to a sorted slice of characters
-        chars := strings.Split(str, "")
-        sort.Strings(chars)
-        sortedStr := strings.Join(chars, "")
-
-        // Add the string to the corresponding group in the map
+        sortedStr := sortString(str)
         groups[sortedStr] = append(groups[sortedStr], str)
     }
 
-    // Convert the map values to a slice of slices
-    var result [][]string
+    result := make([][]string, 0, len(groups))
     for _, group := range groups {
         result = append(result, group)
     }
 
     return result
+    
 }
-
-func main() {
-    strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
-    result := groupAnagrams(strs)
-    fmt.Println(result)
-}
-
