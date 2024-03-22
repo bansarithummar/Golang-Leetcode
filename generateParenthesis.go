0813148f8a425c6df1bb65317975f956681bf1da
Bansari Thummar <bansarithummar@gmail.com>
2024-03-21 20:40:46 -0500

    Update generateParenthesis.go

--- generateParenthesis.go
+++ generateParenthesis.go
@@ -1,39 +1,21 @@
 22. Generate Parentheses
 
-package main
-
-import "fmt"
-
 func generateParenthesis(n int) []string {
-	var res []string
-	var stack []rune
-
-	var backtrack func(openN, closedN int)
-	backtrack = func(openN, closedN int) {
-		if openN == n && closedN == n {
-			res = append(res, string(stack))
-			return
-		}
-
-		if openN < n {
-			stack = append(stack, '(')
-			backtrack(openN+1, closedN)
-			stack = stack[:len(stack)-1]
-		}
-
-		if closedN < openN {
-			stack = append(stack, ')')
-			backtrack(openN, closedN+1)
-			stack = stack[:len(stack)-1]
-		}
-	}
-
-	backtrack(0, 0)
-	return res
+var result []string
+    backtrack(&result, "", 0, 0, n)
+    return result
 }
 
-func main() {
-	n := 3
-	result := generateParenthesis(n)
-	fmt.Println("Result:", result)
+func backtrack(result *[]string, cur string, open, close, max int) {
+    if len(cur) == max*2 {
+        *result = append(*result, cur)
+        return
+    }
+    
+    if open < max {
+        backtrack(result, cur+"(", open+1, close, max)
+    }
+    if close < open {
+        backtrack(result, cur+")", open, close+1, max)
+    }   
 }
