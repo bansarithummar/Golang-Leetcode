674473ebc47bf4294219e7e36bed3db1a9bba19e
Bansari Thummar <bansarithummar@gmail.com>
2024-03-21 20:38:54 -0500

    Update findMin.go

--- findMin.go
+++ findMin.go
@@ -1,34 +1,29 @@
 153. Find Minimum in Rotated Sorted Array
 
-package main
-
-import (
-	"fmt"
-)
-
 func findMin(nums []int) int {
-	res := nums[0]
-	left, right := 0, len(nums)-1
-
-	for left <= right {
-		if nums[left] < nums[right] {
-			res = min(res, nums[left])
-			break
-		}
-		mid := (left + right) / 2
-		res = min(res, nums[mid])
-		if nums[mid] >= nums[left] {
-			left = mid + 1
-		} else {
-			right = mid - 1
-		}
-	}
-	return res
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+    left, right := 0, len(nums)-1
+    if len(nums) == 1 || nums[right] > nums[0] {
+        return nums[0]
+    }
+    
+    // Binary search way
+    for right >= left {
+        mid := left + (right-left)/2
+        // If the mid element is greater than its next element then mid+1 element is the smallest
+        // This point is the point of change from bigger to smaller
+        if nums[mid] > nums[mid+1] {
+            return nums[mid+1]
+        }
+        // If the mid element is lesser than its previous element then mid element is the smallest
+        if nums[mid-1] > nums[mid] {
+            return nums[mid]
+        }
+        // Decide which side to go.
+        if nums[mid] > nums[0] {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return -1
 }
