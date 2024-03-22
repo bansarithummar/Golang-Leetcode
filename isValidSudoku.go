92054460af33fe960d55eeaa558028c24eb266d4
Bansari Thummar <bansarithummar@gmail.com>
2024-03-21 20:42:39 -0500

    Update isValidSudoku.go

--- isValidSudoku.go
+++ isValidSudoku.go
@@ -1,52 +1,32 @@
 36. Valid Sudoku
 
-package main
-
-import (
-    "fmt"
-)
-
 func isValidSudoku(board [][]byte) bool {
-    cols := make([]map[byte]bool, 9)
-    rows := make([]map[byte]bool, 9)
-    squares := make([]map[byte]bool, 9)
-
+    rowChecks := make([]map[byte]bool, 9)
+    colChecks := make([]map[byte]bool, 9)
+    boxChecks := make([]map[byte]bool, 9)
     for i := 0; i < 9; i++ {
-        cols[i] = make(map[byte]bool)
-        rows[i] = make(map[byte]bool)
-        squares[i] = make(map[byte]bool)
+        rowChecks[i] = make(map[byte]bool)
+        colChecks[i] = make(map[byte]bool)
+        boxChecks[i] = make(map[byte]bool)
     }
 
-    for r := 0; r < 9; r++ {
-        for c := 0; c < 9; c++ {
-            if board[r][c] == '.' {
+    for i := 0; i < 9; i++ {
+        for j := 0; j < 9; j++ {
+            num := board[i][j]
+            if num == '.' {
                 continue
             }
-            if rows[r][board[r][c]] || cols[c][board[r][c]] || squares[(r/3)*3+c/3][board[r][c]] {
+            boxIndex := (i/3)*3 + j/3
+
+            if rowChecks[i][num] || colChecks[j][num] || boxChecks[boxIndex][num] {
                 return false
             }
-            rows[r][board[r][c]] = true
-            cols[c][board[r][c]] = true
-            squares[(r/3)*3+c/3][board[r][c]] = true
+
+            rowChecks[i][num] = true
+            colChecks[j][num] = true
+            boxChecks[boxIndex][num] = true
         }
     }
 
-    return true
+    return true    
 }
-
-func main() {
-    board := [][]byte{
-        []byte{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-        []byte{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-        []byte{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-        []byte{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-        []byte{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-        []byte{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-        []byte{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-        []byte{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-        []byte{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
-    }
-
-    fmt.Println(isValidSudoku(board)) // Output: true
-}
-
