f4a72c756f671bf308456d77505dcd2a75389e50
Bansari Thummar <bansarithummar@gmail.com>
2024-03-26 20:28:00 -0500

    Update findLeastNumOfUniqueInts.go

--- findLeastNumOfUniqueInts.go
+++ findLeastNumOfUniqueInts.go
@@ -1,27 +1,17 @@
 1481. Least Number of Unique Integers after K Removals
 
-import (
-    "sort"
-)
-
 func findLeastNumOfUniqueInts(arr []int, k int) int {
     freqMap := make(map[int]int)
     for _, num := range arr {
         freqMap[num]++
     }
-
-    // Convert the frequency map to a slice of pairs (num, freq)
     sortedFreq := make([][2]int, 0, len(freqMap))
     for num, freq := range freqMap {
         sortedFreq = append(sortedFreq, [2]int{num, freq})
     }
-
-    // Sort the slice based on frequency
     sort.Slice(sortedFreq, func(i, j int) bool {
         return sortedFreq[i][1] < sortedFreq[j][1]
     })
-
-    // Remove elements based on k until k becomes 0 or there are no more elements to remove
     for _, pair := range sortedFreq {
         if k >= pair[1] {
             k -= pair[1]
@@ -30,7 +20,5 @@ func findLeastNumOfUniqueInts(arr []int, k int) int {
             break
         }
     }
-
-    // The remaining elements in freqMap represent the least number of unique integers
     return len(freqMap)
 }
