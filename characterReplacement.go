8cca00b3efa8c2c87b4508795c134f8098e9c65e
Bansari Thummar <bansarithummar@gmail.com>
2024-03-31 13:16:55 -0500

    Update characterReplacement.go

--- characterReplacement.go
+++ characterReplacement.go
@@ -10,14 +10,10 @@ func characterReplacement(s string, k int) int {
         count[s[right]]++
         maxCount = max(maxCount, count[s[right]])
 
-        // If the current window size minus the frequency of the most frequent character
-        // is greater than k, shrink the window from the left.
         if right-left+1-maxCount > k {
             count[s[left]]--
             left++
         }
-
-        // Update maxLen if the current window size is larger.
         maxLen = max(maxLen, right-left+1)
     }
 
