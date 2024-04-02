93b4ba3b4750f676747c51fd46eedda0ab27627e
Bansari Thummar <bansarithummar@gmail.com>
2024-04-01 21:47:58 -0500

    Update twoSum.go

--- twoSum.go
+++ twoSum.go
@@ -3,16 +3,12 @@
 func twoSum(nums []int, target int) []int {
     numMap := make(map[int]int)
 
-    // Iterate over the array
     for i, num := range nums {
         complement := target - num
-        if j, ok := numMap[complement]; ok {
-            // If the complement is found in the map, return the indices
+        if j, ok := numMap[complement]; o                                         
             return []int{j, i}
         }
-        // Store the index of the current number in the map
         numMap[num] = i
     }
-    // In case no solution is found, though the problem guarantees one.
     return nil    
 }
