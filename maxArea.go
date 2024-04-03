6cf199c477c8ff386ad7fe54445b22a3fca78e80
Bansari Thummar <bansarithummar@gmail.com>
2024-04-03 17:56:35 -0500

    Update maxArea.go

--- maxArea.go
+++ maxArea.go
@@ -1,11 +1,5 @@
 11. Container With Most Water
 
-package main
-
-import (
-	"fmt"
-)
-
 func maxArea(height []int) int {
 	left, right := 0, len(height)-1
 	max := 0
@@ -30,9 +24,4 @@ func min(a, b int) int {
 		return a
 	}
 	return b
-}
-
-func main() {
-	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
-	fmt.Println(maxArea(height)) // Output: 49
 }
