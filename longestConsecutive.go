9a4b2cf736552a1c7fd2d2fe8f6d8161ac15f385
Bansari Thummar <bansarithummar@gmail.com>
2024-04-05 12:50:02 -0500

    Update longestConsecutive.go

--- longestConsecutive.go
+++ longestConsecutive.go
@@ -3,22 +3,15 @@
 func longestConsecutive(nums []int) int {
 	numSet := make(map[int]bool)
 	longest := 0
-
-	// Populate the set with nums
 	for _, num := range nums {
 		numSet[num] = true
 	}
-
-	// Iterate through nums
 	for _, num := range nums {
-		// If num-1 is not in numSet, start counting consecutive numbers
 		if !numSet[num-1] {
 			length := 0
-			// Count consecutive numbers
 			for numSet[num+length] {
 				length++
 			}
-			// Update longest consecutive sequence
 			if length > longest {
 				longest = length
 			}
