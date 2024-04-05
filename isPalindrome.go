a8843c8390e56dfe27437b032d7faa821845a6e5
Bansari Thummar <bansarithummar@gmail.com>
2024-04-05 12:43:44 -0500

    Update isPalindrome.go

--- isPalindrome.go
+++ isPalindrome.go
@@ -1,19 +1,16 @@
 125. Valid Palindrome
 
 func isPalindrome(s string) bool {
-    s = strings.ToLower(s) // Convert to lowercase
+    s = strings.ToLower(s) 
     left, right := 0, len(s)-1
 
     for left < right {
-        // Move left index forward if not alphanumeric
         for left < right && !unicode.IsLetter(rune(s[left])) && !unicode.IsNumber(rune(s[left])) {
             left++
         }
-        // Move right index backward if not alphanumeric
         for left < right && !unicode.IsLetter(rune(s[right])) && !unicode.IsNumber(rune(s[right])) {
             right--
         }
-        // Compare characters
         if s[left] != s[right] {
             return false
         }
