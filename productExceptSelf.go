ccda4d6cd790ec7631504496ad15af6de75e7f26
Bansari Thummar <bansarithummar@gmail.com>
2024-04-06 19:13:57 -0500

    Update productExceptSelf.go

--- productExceptSelf.go
+++ productExceptSelf.go
@@ -4,15 +4,11 @@
 func productExceptSelf(nums []int) []int {
     n := len(nums)
     res := make([]int, n)
-
-    // Calculate prefix products
     prefix := 1
     for i := 0; i < n; i++ {
         res[i] = prefix
         prefix *= nums[i]
     }
-
-    // Calculate postfix products while updating the result
     postfix := 1
     for i := n - 1; i >= 0; i-- {
         res[i] *= postfix
