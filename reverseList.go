ed4537a975274499dfb6f333b327d18fed3bfe4f
Bansari Thummar <bansarithummar@gmail.com>
2024-04-23 16:50:39 -0500

    Update reverseList.go

--- reverseList.go
+++ reverseList.go
@@ -1,1 +1,16 @@
+206. Reverse Linked List
 
+
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode = nil
+	current := head
+
+	for current != nil {
+		nextTemp := current.Next
+		current.Next = prev
+		prev = current
+		current = nextTemp
+	}
+
+	return prev
+}
