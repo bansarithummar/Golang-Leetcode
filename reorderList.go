036dda30eff6de3f9efe2396c4137c72f027d949
Bansari Thummar <bansarithummar@gmail.com>
2024-04-26 16:22:50 -0500

    Update reorderList.go

--- reorderList.go
+++ reorderList.go
@@ -1,1 +1,32 @@
 143. Reorder List
+
+
+func reorderList(head *ListNode)  {
+    if head == nil || head.Next == nil {
+        return
+    }
+    slow, fast := head, head
+    for fast.Next != nil && fast.Next.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    middle := slow.Next
+    slow.Next = nil
+    var prev *ListNode
+    for middle != nil {
+        next := middle.Next
+        middle.Next = prev
+        prev = middle
+        middle = next
+    }
+    reversed := prev
+    current := head
+    for current != nil && reversed != nil {
+        next1 := current.Next
+        next2 := reversed.Next
+        current.Next = reversed
+        reversed.Next = next1
+        current = next1
+        reversed = next2
+    }    
+}
