e6a22d6d0352bd959c8ce425773ac164ea71f494
Bansari Thummar <bansarithummar@gmail.com>
2024-04-29 12:22:20 -0500

    Update removeNthFromEnd.go

--- removeNthFromEnd.go
+++ removeNthFromEnd.go
@@ -1,1 +1,20 @@
+19. Remove Nth Node From End of List
 
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    dummy := &ListNode{Val: 0, Next: head}
+    first := head
+    length := 0
+    for first != nil {
+        length++
+        first = first.Next
+    }
+    length -= n
+    first = dummy
+    for length > 0 {
+        length--
+        first = first.Next
+    }
+    first.Next = first.Next.Next
+    return dummy.Next
+}
