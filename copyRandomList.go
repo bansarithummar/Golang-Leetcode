aa5a25db0bdfcf3360694e90f70b91556c186f13
Bansari Thummar <bansarithummar@gmail.com>
2024-04-30 15:00:09 -0500

    Update copyRandomList.go

--- copyRandomList.go
+++ copyRandomList.go
@@ -1,1 +1,22 @@
 138. Copy List with Random Pointer
+
+
+func copyRandomList(head *Node) *Node {
+    if head == nil {
+        return nil
+    }
+    nodeMap := make(map[*Node]*Node)
+    curr := head
+    for curr != nil {
+        nodeMap[curr] = &Node{Val: curr.Val}
+        curr = curr.Next
+    }
+    curr = head
+    for curr != nil {
+        newNode := nodeMap[curr]
+        newNode.Next = nodeMap[curr.Next]
+        newNode.Random = nodeMap[curr.Random]
+        curr = curr.Next
+    }
+    return nodeMap[head]    
+}
