f3ee79d012fd8337961e6cf04f8a505a517a05b9
Bansari Thummar <bansarithummar@gmail.com>
2024-05-02 20:16:16 -0500

    Update hasCycle.go

--- hasCycle.go
+++ hasCycle.go
@@ -1,1 +1,17 @@
 141. Linked List Cycle
+
+
+func hasCycle(head *ListNode) bool {
+    if head == nil || head.Next == nil {
+        return false
+    }
+    slow, fast := head, head.Next
+    for slow != fast {
+        if fast == nil || fast.Next == nil {
+            return false
+        }
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return true    
+}
