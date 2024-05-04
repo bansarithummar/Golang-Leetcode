592aae5b69534c1dc9373946cee53c0047b746cd
Bansari Thummar <bansarithummar@gmail.com>
2024-05-04 18:48:59 -0500

    Update findDuplicate.go

--- findDuplicate.go
+++ findDuplicate.go
@@ -1,1 +1,22 @@
+287. Find the Duplicate Number
 
+
+func findDuplicate(nums []int) int {
+    tortoise := nums[0]
+    hare := nums[0]
+    for {
+        tortoise = nums[tortoise]
+        hare = nums[nums[hare]]
+        if tortoise == hare {
+            break
+        }
+    }
+    ptr1 := nums[0]
+    ptr2 := tortoise
+    for ptr1 != ptr2 {
+        ptr1 = nums[ptr1]
+        ptr2 = nums[ptr2]
+    }
+    
+    return ptr1    
+}
