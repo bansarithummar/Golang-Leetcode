d632003c8f2db34ea6f6cd749316be6afc02cc93
Bansari Thummar <bansarithummar@gmail.com>
2024-05-14 22:07:43 -0500

    Update invertTree.go

--- invertTree.go
+++ invertTree.go
@@ -1,1 +1,10 @@
 226. Invert Binary Tree
+
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+    
+    return root
+}
