65a48852aa02fe3504a13f8bdda8233ca98fad5c
Bansari Thummar <bansarithummar@gmail.com>
2024-05-21 17:46:34 -0500

    Update lowestCommonAncestor.go

--- lowestCommonAncestor.go
+++ lowestCommonAncestor.go
@@ -1,1 +1,14 @@
+235. Lowest Common Ancestor of a Binary Search tree
 
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    for root != nil {
+        if p.Val > root.Val && q.Val > root.Val {
+            root = root.Right
+        } else if p.Val < root.Val && q.Val < root.Val {
+            root = root.Left
+        } else {
+            return root
+        }
+    }
+    return nil
+}
