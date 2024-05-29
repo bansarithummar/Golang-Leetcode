de83c93ce8fc023817a3d6b43fbf670feb267f39
Bansari Thummar <bansarithummar@gmail.com>
2024-05-28 21:03:14 -0500

    Update 0004-Median-of-Two-Sorted-Arrays.go

--- 0004-Median-of-Two-Sorted-Arrays.go
+++ 0004-Median-of-Two-Sorted-Arrays.go
@@ -1,45 +1,48 @@
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
     if len(nums1) > len(nums2) {
-    nums1, nums2 = nums2, nums1
-    }
+		return findMedianSortedArrays(nums2, nums1)
+	}
 
-    m, n := len(nums1), len(nums2)
-    imin, imax, halfLen := 0, m, (m+n+1)/2
+	m, n := len(nums1), len(nums2)
+	total := m + n
+	half := (total + 1) / 2
 
-    for imin <= imax {
-        i := (imin + imax) / 2
-        j := halfLen - i
+	l, r := 0, m
 
-        if i < imax && nums2[j-1] > nums1[i] {
-            imin = i + 1
-        } else if i > imin && nums1[i-1] > nums2[j] {
-            imax = i - 1
-        } else {
-            // i is perfect
-            var maxLeft float64
-            if i == 0 {
-                maxLeft = float64(nums2[j-1])
-            } else if j == 0 {
-                maxLeft = float64(nums1[i-1])
-            } else {
-                maxLeft = float64(math.Max(float64(nums1[i-1]), float64(nums2[j-1])))
-            }
-            if (m+n)%2 == 1 {
-                return maxLeft // Odd case
-            }
+	for l <= r {
+		i := (l + r) / 2
+		j := half - i
 
-            var minRight float64
-            if i == m {
-                minRight = float64(nums2[j])
-            } else if j == n {
-                minRight = float64(nums1[i])
-            } else {
-                minRight = float64(math.Min(float64(nums1[i]), float64(nums2[j])))
-            }
+		nums1Left := math.Inf(-1)
+		if i > 0 {
+			nums1Left = float64(nums1[i-1])
+		}
+		nums1Right := math.Inf(1)
+		if i < m {
+			nums1Right = float64(nums1[i])
+		}
 
-            return (maxLeft + minRight) / 2.0 // Even case
-        }
-    }
+		nums2Left := math.Inf(-1)
+		if j > 0 {
+			nums2Left = float64(nums2[j-1])
+		}
+		nums2Right := math.Inf(1)
+		if j < n {
+			nums2Right = float64(nums2[j])
+		}
 
-    return 0.0 	
+		if nums1Left <= nums2Right && nums2Left <= nums1Right {
+			if total%2 == 0 {
+				return (math.Max(nums1Left, nums2Left) + math.Min(nums1Right, nums2Right)) / 2
+			} else {
+				return math.Max(nums1Left, nums2Left)
+			}
+		} else if nums1Left > nums2Right {
+			r = i - 1
+		} else {
+			l = i + 1
+		}
+	}
+
+	panic("Should not reach here")    
 }
