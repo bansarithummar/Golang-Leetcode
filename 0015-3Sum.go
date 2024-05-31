a2d78be7cb6309331a3b6af5513a1ed23941e186
Bansari Thummar <bansarithummar@gmail.com>
2024-05-30 20:41:44 -0500

    Update 0015-3Sum.go

--- 0015-3Sum.go
+++ 0015-3Sum.go
@@ -1,30 +1,36 @@
-15. 3Sum
-
-
 func threeSum(nums []int) [][]int {
-	var res [][]int
-	sort.Ints(nums)
+    sort.Ints(nums) 
+	var result [][]int 
 
 	for i := 0; i < len(nums)-2; i++ {
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
-		l, r := i+1, len(nums)-1
-		for l < r {
-			threeSum := nums[i] + nums[l] + nums[r]
-			if threeSum > 0 {
-				r--
-			} else if threeSum < 0 {
-				l++
-			} else {
-				res = append(res, []int{nums[i], nums[l], nums[r]})
-				l++
-				for nums[l] == nums[l-1] && l < r {
-					l++
+
+		left, right := i+1, len(nums)-1 
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right] 
+			switch {
+			case sum == 0:
+				result = append(result, []int{nums[i], nums[left], nums[right]}) 
+				left++
+				right--
+				
+				for left < right && nums[left] == nums[left-1] {
+					left++
 				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			case sum < 0:
+				left++ 
+			default:
+				right-- 
 			}
 		}
 	}
-	return res
-}
 
+	return result
+    
+}
