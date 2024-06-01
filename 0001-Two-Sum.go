3ee767d10d28c84785be924816ce41a13b6b6708
Bansari Thummar <bansarithummar@gmail.com>
2024-06-01 12:10:25 -0500

    Update 0001-Two-Sum.go

--- 0001-Two-Sum.go
+++ 0001-Two-Sum.go
@@ -1,14 +1,12 @@
-1. Two Sum
-
 func twoSum(nums []int, target int) []int {
-    numMap := make(map[int]int)
+    prevMap := make(map[int]int)
 
     for i, num := range nums {
-        complement := target - num
-        if j, ok := numMap[complement]; o                                         
+        diff := target - num
+        if j, found := prevMap[diff]; found {
             return []int{j, i}
         }
-        numMap[num] = i
+        prevMap[num] = i
     }
     return nil    
 }
