9d635ee978924e7b8a879d2e877e51b329f14dc7
Bansari Thummar <bansarithummar@gmail.com>
2024-06-01 12:15:23 -0500

    Update 0020-Valid-Parentheses.go

--- 0020-Valid-Parentheses.go
+++ 0020-Valid-Parentheses.go
@@ -1,31 +1,21 @@
-20. Valid Parentheses
-
-
 func isValid(s string) bool {
     stack := []rune{}
+	brackets := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
 
-    for _, char := range s {
-        switch char {
-        case '(', '{', '[':
-            stack = append(stack, char)
-        case ')':
-            if len(stack) == 0 || stack[len(stack)-1] != '(' {
-                return false
-            }
-            stack = stack[:len(stack)-1]
-        case '}':
-            if len(stack) == 0 || stack[len(stack)-1] != '{' {
-                return false
-            }
-            stack = stack[:len(stack)-1]
-        case ']':
-            if len(stack) == 0 || stack[len(stack)-1] != '[' {
-                return false
-            }
-            stack = stack[:len(stack)-1]
-        }
-    }
-
-    return len(stack) == 0    
+	for _, char := range s {
+		switch char {
+		case '(', '{', '[':
+			stack = append(stack, char)
+		case ')', '}', ']':
+			if len(stack) == 0 || stack[len(stack)-1] != brackets[char] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0    
 }
-
