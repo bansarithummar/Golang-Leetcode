d5bc2c5e9b9bf187266febc4275ecf3438909198
Bansari Thummar <bansarithummar@gmail.com>
2024-06-02 19:55:52 -0500

    Update 0033-Search-in-Rotated-Sorted-Array.go

--- 0033-Search-in-Rotated-Sorted-Array.go
+++ 0033-Search-in-Rotated-Sorted-Array.go
@@ -1,29 +1,25 @@
-33. Search in Rotated Sorted Array
-
-
 func search(nums []int, target int) int {
-	left, right := 0, len(nums)-1
+    left, right := 0, len(nums) - 1
 
-	for left <= right {
-		mid := (left + right) / 2
-		if target == nums[mid] {
-			return mid
-		}
-
-		if nums[left] <= nums[mid] {
-			if target > nums[mid] || target < nums[left] {
-				left = mid + 1
-			} else {
-				right = mid - 1
-			}
-		} else {
-			if target < nums[mid] || target > nums[right] {
-				right = mid - 1
-			} else {
-				left = mid + 1
-			}
-		}
-	}
-	return -1
+    for left <= right {
+        mid := left + (right - left) / 2
+        
+        if nums[mid] == target {
+            return mid
+        }
+        if nums[left] <= nums[mid] {
+            if nums[left] <= target && target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[right] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+    return -1   
 }
-
