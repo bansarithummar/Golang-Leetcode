dcc724e10551174ab0b790df9b4bcf3d831b94bc
Bansari Thummar <bansarithummar@gmail.com>
2024-06-02 19:53:29 -0500

    Update 0022-Generate-Parentheses.go

--- 0022-Generate-Parentheses.go
+++ 0022-Generate-Parentheses.go
@@ -1,22 +1,19 @@
-22. Generate Parentheses
-
-
 func generateParenthesis(n int) []string {
-var result []string
-    backtrack(&result, "", 0, 0, n)
-    return result
+    var result []string
+	var current string
+	backtrack(n, 0, 0, current, &result)
+	return result
 }
 
-func backtrack(result *[]string, cur string, open, close, max int) {
-    if len(cur) == max*2 {
-        *result = append(*result, cur)
-        return
-    }
-    
-    if open < max {
-        backtrack(result, cur+"(", open+1, close, max)
-    }
-    if close < open {
-        backtrack(result, cur+")", open, close+1, max)
-    }   
+func backtrack(n, open, close int, current string, result *[]string) {
+	if len(current) == 2*n {
+		*result = append(*result, current)
+		return
+	}
+	if open < n {
+		backtrack(n, open+1, close, current+"(", result)
+	}
+	if close < open {
+		backtrack(n, open, close+1, current+")", result)
+	}    
 }
