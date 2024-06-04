8eb41c9d102171cd9a9201b75bf16bee18affb2f
Bansari Thummar <bansarithummar@gmail.com>
2024-06-04 18:06:48 -0500

    Update 0076-Minimum-Window-Substring.go

--- 0076-Minimum-Window-Substring.go
+++ 0076-Minimum-Window-Substring.go
@@ -1,44 +1,40 @@
-76. Minimum Window Substring
-
-
 func minWindow(s string, t string) string {
-	if len(s) == 0 || len(t) == 0 || len(s) < len(t) {
+    if len(t) == 0 || len(s) == 0 {
 		return ""
 	}
-
-	mapS := make([]int, 128)
-	count := len(t)
-	start, end := 0, 0
-	minLen, startIndex := int(^uint(0)>>1), 0
-    /// UPVOTE !
-	for _, char := range t {
-		mapS[char]++
+	tCount := make(map[byte]int)
+	for i := 0; i < len(t); i++ {
+		tCount[t[i]]++
 	}
+	windowCount := make(map[byte]int)
+	required := len(tCount)
+	left, right, formed := 0, 0, 0
+	ans := [3]int{-1, 0, 0} 
 
-	for end < len(s) {
-		if mapS[s[end]] > 0 {
-			count--
+	for right < len(s) {
+		c := s[right]
+		windowCount[c]++
+		
+		if tCount[c] > 0 && windowCount[c] == tCount[c] {
+			formed++
 		}
-		mapS[s[end]]--
-		end++
-
-		for count == 0 {
-			if end-start < minLen {
-				startIndex = start
-				minLen = end - start
+		for left <= right && formed == required {
+			c = s[left]
+			if ans[0] == -1 || right-left+1 < ans[0] {
+				ans = [3]int{right-left+1, left, right}
 			}
 
-			if mapS[s[start]] == 0 {
-				count++
+			windowCount[c]--
+			if tCount[c] > 0 && windowCount[c] < tCount[c] {
+				formed--
 			}
-			mapS[s[start]]++
-			start++
+			left++
 		}
+		right++
 	}
 
-	if minLen == int(^uint(0)>>1) {
+	if ans[0] == -1 {
 		return ""
 	}
-
-	return s[startIndex : startIndex+minLen]
+	return s[ans[1] : ans[2]+1]    
 }
