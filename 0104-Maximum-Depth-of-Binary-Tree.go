e009c73cfc2cf033ca1e9398dbaff217a0c91926
Bansari Thummar <bansarithummar@gmail.com>
2024-06-04 18:13:05 -0500

    Update 0104-Maximum-Depth-of-Binary-Tree.go

--- 0104-Maximum-Depth-of-Binary-Tree.go
+++ 0104-Maximum-Depth-of-Binary-Tree.go
@@ -1,5 +1,11 @@
-104. Maximum Depth of Binary Tree
-
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func maxDepth(root *TreeNode) int {
     if root == nil {
         return 0
