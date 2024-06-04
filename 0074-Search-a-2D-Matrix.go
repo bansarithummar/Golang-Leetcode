5f80a3fd9c2e1c1705338023115f63e75b879439
Bansari Thummar <bansarithummar@gmail.com>
2024-06-03 22:28:57 -0500

    Update 0074-Search-a-2D-Matrix.go

--- 0074-Search-a-2D-Matrix.go
+++ 0074-Search-a-2D-Matrix.go
@@ -1,37 +1,22 @@
-74. Search a 2D Matrix
-
-
 func searchMatrix(matrix [][]int, target int) bool {
-	rows, cols := len(matrix), len(matrix[0])
-	top, bot := 0, rows-1
+    if len(matrix) == 0 || len(matrix[0]) == 0 {
+        return false
+    }
 
-	for top <= bot {
-		row := (top + bot) / 2
-		if target > matrix[row][cols-1] {
-			top = row + 1
-		} else if target < matrix[row][0] {
-			bot = row - 1
-		} else {
-			break
-		}
-	}
+    m, n := len(matrix), len(matrix[0])
+    left, right := 0, m*n-1
 
-	if !(top <= bot) {
-		return false
-	}
+    for left <= right {
+        mid := left + (right - left) / 2
+        midValue := matrix[mid/n][mid%n]
 
-	row := (top + bot) / 2
-	left, right := 0, cols-1
-	for left <= right {
-		mid := (left + right) / 2
-		if target > matrix[row][mid] {
-			left = mid + 1
-		} else if target < matrix[row][mid] {
-			right = mid - 1
-		} else {
-			return true
-		}
-	}
-	return false
+        if midValue == target {
+            return true
+        } else if midValue < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return false
 }
-
