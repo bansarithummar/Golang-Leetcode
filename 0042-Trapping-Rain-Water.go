bcd7a68cfe1e5969ce806487cab37f9ff68718b4
Bansari Thummar <bansarithummar@gmail.com>
2024-06-03 22:25:06 -0500

    Update 0042-Trapping-Rain-Water.go

--- 0042-Trapping-Rain-Water.go
+++ 0042-Trapping-Rain-Water.go
@@ -1,31 +1,29 @@
-42. Trapping Rain Water
-
-
 func trap(height []int) int {
-	if len(height) == 0 {
+    if len(height) == 0 {
 		return 0
 	}
+
 	left, right := 0, len(height)-1
 	leftMax, rightMax := height[left], height[right]
-	res := 0
+	waterTrapped := 0
+
 	for left < right {
-		if leftMax < rightMax {
+		if height[left] <= height[right] {
+			if height[left] >= leftMax {
+				leftMax = height[left]
+			} else {
+				waterTrapped += leftMax - height[left]
+			}
 			left++
-			leftMax = max(leftMax, height[left])
-			res += leftMax - height[left]
 		} else {
+			if height[right] >= rightMax {
+				rightMax = height[right]
+			} else {
+				waterTrapped += rightMax - height[right]
+			}
 			right--
-			rightMax = max(rightMax, height[right])
-			res += rightMax - height[right]
 		}
 	}
 
-	return res
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return waterTrapped    
 }
