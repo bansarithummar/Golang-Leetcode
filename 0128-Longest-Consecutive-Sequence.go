c95e4fec052ebe9aea178c024896ad71e9776375
Bansari Thummar <bansarithummar@gmail.com>
2024-06-05 13:34:54 -0500

    Update 0128-Longest-Consecutive-Sequence.go

--- 0128-Longest-Consecutive-Sequence.go
+++ 0128-Longest-Consecutive-Sequence.go
@@ -1,23 +1,24 @@
-128. Longest Consecutive Sequence
-
-
 func longestConsecutive(nums []int) int {
-	numSet := make(map[int]bool)
-	longest := 0
-	for _, num := range nums {
-		numSet[num] = true
-	}
-	for _, num := range nums {
-		if !numSet[num-1] {
-			length := 0
-			for numSet[num+length] {
-				length++
-			}
-			if length > longest {
-				longest = length
-			}
-		}
-	}
-
-	return longest
+    if len(nums) == 0 {
+        return 0
+    }
+    numSet := make(map[int]bool)
+    for _, num := range nums {
+        numSet[num] = true
+    }
+    longestStreak := 0
+    for num := range numSet {
+        if !numSet[num-1] {
+            currentNum := num
+            currentStreak := 1
+            for numSet[currentNum+1] {
+                currentNum += 1
+                currentStreak += 1
+            }
+            if currentStreak > longestStreak {
+                longestStreak = currentStreak
+            }
+        }
+    }
+    return longestStreak
 }
