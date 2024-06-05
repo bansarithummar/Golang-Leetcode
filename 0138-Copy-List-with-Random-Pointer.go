1de53910a23bd6df6d84ee5a599f3e24e1127914
Bansari Thummar <bansarithummar@gmail.com>
2024-06-05 13:39:31 -0500

    Update 0138-Copy-List-with-Random-Pointer.go

--- 0138-Copy-List-with-Random-Pointer.go
+++ 0138-Copy-List-with-Random-Pointer.go
@@ -1,23 +1,38 @@
 /**
- * Definition for singly-linked list.
- * type ListNode struct {
+ * Definition for a Node.
+ * type Node struct {
  *     Val int
- *     Next *ListNode
+ *     Next *Node
+ *     Random *Node
  * }
  */
-func hasCycle(head *ListNode) bool {
-if head == nil || head.Next == nil {
-		return false
-	}
-	slow := head
-	fast := head.Next
 
-	for slow != fast {
-		if fast == nil || fast.Next == nil {
-			return false
-		}
-		slow = slow.Next
-		fast = fast.Next.Next
+func copyRandomList(head *Node) *Node {
+    if head == nil {
+		return nil
 	}
-	return true    
+	current := head
+	for current != nil {
+		newNode := &Node{Val: current.Val, Next: current.Next}
+		current.Next = newNode
+		current = newNode.Next
+	}
+	current = head
+	for current != nil {
+		if current.Random != nil {
+			current.Next.Random = current.Random.Next
+		}
+		current = current.Next.Next
+	}
+	current = head
+	copyHead := head.Next
+	for current != nil {
+		copyNode := current.Next
+		current.Next = copyNode.Next
+		if copyNode.Next != nil {
+			copyNode.Next = copyNode.Next.Next
+		}
+		current = current.Next
+	}
+	return copyHead
 }
