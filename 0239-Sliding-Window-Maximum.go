aa7114369183fdf47e5e0b1a6a2f1e3643cb2871
Bansari Thummar <bansarithummar@gmail.com>
2024-06-08 13:29:22 -0500

    Update 0239-Sliding-Window-Maximum.go

--- 0239-Sliding-Window-Maximum.go
+++ 0239-Sliding-Window-Maximum.go
@@ -1,25 +1,21 @@
-239. Sliding Window Maximum
-
-
 func maxSlidingWindow(nums []int, k int) []int {
-    result := make([]int, 0)
-    deque := make([]int, 0)
-    add := func(i int) {
-        for len(deque) > 0 && nums[deque[len(deque)-1]] <= nums[i] {
-            deque = deque[:len(deque)-1]
-        }
-        deque = append(deque, i)
-    }
-    for i := 0; i < k; i++ {
-        add(i)
-    }
-    result = append(result, nums[deque[0]])
-    for i := k; i < len(nums); i++ {
-        if deque[0] == i-k {
-            deque = deque[1:]
-        }
-        add(i)
-        result = append(result, nums[deque[0]])
-    }
-    return result    
+    if len(nums) == 0 {
+		return []int{}
+	}
+	deque := []int{}
+	result := []int{}
+
+	for i := 0; i < len(nums); i++ {
+		if len(deque) > 0 && deque[0] < i-k+1 {
+			deque = deque[1:]
+		}
+		for len(deque) > 0 && nums[deque[len(deque)-1]] < nums[i] {
+			deque = deque[:len(deque)-1]
+		}
+		deque = append(deque, i)
+		if i >= k-1 {
+			result = append(result, nums[deque[0]])
+		}
+	}
+	return result    
 }
