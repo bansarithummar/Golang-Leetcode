1c00d90a50bc1b358cec9c01cdc4f9ca6ddfc5e3
Bansari Thummar <bansarithummar@gmail.com>
2024-06-08 13:30:57 -0500

    Update 0242-Valid-Anagram.go

--- 0242-Valid-Anagram.go
+++ 0242-Valid-Anagram.go
@@ -1,23 +1,16 @@
-242. Valid Anagram
-
-
 func isAnagram(s string, t string) bool {
     if len(s) != len(t) {
         return false
     }
-
     counts := make(map[rune]int)
-
     for _, c := range s {
         counts[c]++
     }
-
     for _, c := range t {
         counts[c]--
         if counts[c] < 0 {
             return false
         }
     }
-
     return true
 }
