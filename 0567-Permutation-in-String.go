d68b01d81c3369cdfd984304d80d355f40e2ab49
Bansari Thummar <bansarithummar@gmail.com>
2024-06-09 12:54:37 -0500

    Update 0567-Permutation-in-String.go

--- 0567-Permutation-in-String.go
+++ 0567-Permutation-in-String.go
@@ -1,47 +1,40 @@
-567. Permutation in String
-
-
 func checkInclusion(s1 string, s2 string) bool {
     if len(s1) > len(s2) {
         return false
     }
-
-    var s1Count, s2Count [26]int
+    s1Count := [26]int{}
+    s2Count := [26]int{}
     for i := 0; i < len(s1); i++ {
         s1Count[s1[i]-'a']++
         s2Count[s2[i]-'a']++
     }
-
     matches := 0
     for i := 0; i < 26; i++ {
         if s1Count[i] == s2Count[i] {
             matches++
         }
     }
-
-    left := 0
-    for right := len(s1); right < len(s2); right++ {
+    for i := len(s1); i < len(s2); i++ {
         if matches == 26 {
             return true
         }
 
-        index := s2[right] - 'a'
-        s2Count[index]++
-        if s1Count[index] == s2Count[index] {
+        right := s2[i] - 'a'
+        left := s2[i-len(s1)] - 'a'
+
+        s2Count[right]++
+        if s2Count[right] == s1Count[right] {
             matches++
-        } else if s1Count[index]+1 == s2Count[index] {
+        } else if s2Count[right] == s1Count[right]+1 {
             matches--
         }
 
-        index = s2[left] - 'a'
-        s2Count[index]--
-        if s1Count[index] == s2Count[index] {
+        s2Count[left]--
+        if s2Count[left] == s1Count[left] {
             matches++
-        } else if s1Count[index]-1 == s2Count[index] {
+        } else if s2Count[left] == s1Count[left]-1 {
             matches--
         }
-        left++
     }
-
-    return matches == 26
+    return matches == 26   
 }
