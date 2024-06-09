d0eea8743e4ae71fd55bbb5b906ef88f8e805e35
Bansari Thummar <bansarithummar@gmail.com>
2024-06-09 13:10:39 -0500

    Update 0853-Car-Fleet.go

--- 0853-Car-Fleet.go
+++ 0853-Car-Fleet.go
@@ -1,25 +1,23 @@
-853. Car Fleet
-
 func carFleet(target int, position []int, speed []int) int {
-	type pair struct {
-		position int
-		speed    int
+    n := len(position)
+	if n == 0 {
+		return 0
 	}
-	var pairs []pair
-	for i := 0; i < len(position); i++ {
-		pairs = append(pairs, pair{position[i], speed[i]})
+	cars := make([][2]float64, n)
+	for i := 0; i < n; i++ {
+		time := float64(target-position[i]) / float64(speed[i])
+		cars[i] = [2]float64{float64(position[i]), time}
 	}
-	sort.Slice(pairs, func(i, j int) bool {
-		return pairs[i].position > pairs[j].position
+	sort.Slice(cars, func(i, j int) bool {
+		return cars[i][0] > cars[j][0]
 	})
-	var stack []float64
-	for _, p := range pairs {
-		time := float64(target-p.position) / float64(p.speed)
-		stack = append(stack, time)
-		if len(stack) >= 2 && stack[len(stack)-1] <= stack[len(stack)-2] {
-			stack = stack[:len(stack)-1]
+	fleets := 0
+	currentFleetTime := 0.0
+	for i := 0; i < n; i++ {
+		if cars[i][1] > currentFleetTime {
+			fleets++
+			currentFleetTime = cars[i][1]
 		}
 	}
-	return len(stack)
+	return fleets    
 }
-
