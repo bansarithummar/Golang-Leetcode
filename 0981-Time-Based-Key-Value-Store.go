3d4b677390278c86b24e0633250fe3c51687f796
Bansari Thummar <bansarithummar@gmail.com>
2024-06-10 10:24:00 -0500

    Update 0981-Time-Based-Key-Value-Store.go

--- 0981-Time-Based-Key-Value-Store.go
+++ 0981-Time-Based-Key-Value-Store.go
@@ -1,32 +1,38 @@
-981. Time Based Key-Value Store
-
-type TimeMap struct {
-    store map[string][]pair // Maps keys to a list of (value, timestamp) pairs
+type Entry struct {
+	value     string
+	timestamp int
 }
 
-type pair struct {
-    value     string
-    timestamp int
+type TimeMap struct {
+    store map[string][]Entry    
 }
 
 func Constructor() TimeMap {
-    return TimeMap{store: make(map[string][]pair)}
+    return TimeMap{store: make(map[string][]Entry)}    
 }
 
-func (this *TimeMap) Set(key string, value string, timestamp int) {
-    this.store[key] = append(this.store[key], pair{value, timestamp})
+func (this *TimeMap) Set(key string, value string, timestamp int)  {
+    this.store[key] = append(this.store[key], Entry{value, timestamp})    
 }
 
 func (this *TimeMap) Get(key string, timestamp int) string {
-    pairs := this.store[key]
-    i := sort.Search(len(pairs), func(i int) bool {
-        return pairs[i].timestamp > timestamp
-    })
-    if i > 0 {
-        return pairs[i-1].value
-    }
-    return "" 
+    entries, exists := this.store[key]
+	if !exists {
+		return ""
+	}
+	idx := sort.Search(len(entries), func(i int) bool {
+		return entries[i].timestamp > timestamp
+	})
+	if idx == 0 {
+		return ""
+	}
+	return entries[idx-1].value    
 }
 
 
-
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Set(key,value,timestamp);
+ * param_2 := obj.Get(key,timestamp);
+ */
