49896f89a1e92b21b6910d51a85ce22394e6169f
Bansari Thummar <bansarithummar@gmail.com>
2024-06-11 14:07:36 -0500

    Update 0023-Merge-k-Sorted-Lists.go

--- 0023-Merge-k-Sorted-Lists.go
+++ 0023-Merge-k-Sorted-Lists.go
@@ -1,1 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 
+type PriorityQueue []*ListNode
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Val < pq[j].Val
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	*pq = append(*pq, x.(*ListNode))
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+    pq := &PriorityQueue{}
+	heap.Init(pq)
+	for _, list := range lists {
+		if list != nil {
+			heap.Push(pq, list)
+		}
+	}
+	dummy := &ListNode{}
+	current := dummy
+
+	for pq.Len() > 0 {
+		node := heap.Pop(pq).(*ListNode)
+		current.Next = node
+		current = current.Next
+		if node.Next != nil {
+			heap.Push(pq, node.Next)
+		}
+	}
+	return dummy.Next    
+}
