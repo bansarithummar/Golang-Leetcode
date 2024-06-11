adbcf0581439446da4a8b235bf55c3f9c540c83a
Bansari Thummar <bansarithummar@gmail.com>
2024-06-11 14:00:06 -0500

    Update 0271-Encode-and-Decode-Strings.go

--- 0271-Encode-and-Decode-Strings.go
+++ 0271-Encode-and-Decode-Strings.go
@@ -1,1 +1,35 @@
-271. Encode and Decode Strings
+func encode(strs []string) string {
+	var encoded strings.Builder
+
+	for _, str := range strs {
+		length := strconv.Itoa(len(str))
+		encoded.WriteString(length)
+		encoded.WriteString("#")
+		encoded.WriteString(str)
+	}
+
+	return encoded.String()
+}
+
+func decode(s string) []string {
+	var decoded []string
+	i := 0
+
+	for i < len(s) {
+		j := strings.Index(s[i:], "#")
+		if j == -1 {
+			break
+		}
+		j += i 
+		length, err := strconv.Atoi(s[i:j])
+		if err != nil {
+			break
+		}
+		start := j + 1
+		end := start + length
+		decoded = append(decoded, s[start:end])
+		i = end
+	}
+
+	return decoded
+}
