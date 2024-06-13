a81f56eb88430d22486b3372e23543abbfb38277
Bansari Thummar <bansarithummar@gmail.com>
2024-06-12 20:08:13 -0500

    Update 0025-Reverse-Nodes-in-k-Group.go

--- 0025-Reverse-Nodes-in-k-Group.go
+++ 0025-Reverse-Nodes-in-k-Group.go
@@ -1,1 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 
+func reverseKGroup(head *ListNode, k int) *ListNode {
+    if head == nil || k == 1 {
+        return head
+    }
+    reverse := func(start *ListNode, end *ListNode) *ListNode {
+        var prev *ListNode
+        curr := start
+        next := start.Next
+        for curr != end {
+            curr.Next, prev, curr, next = prev, curr, next, next.Next
+        }
+        return prev
+    }
+    count := 0
+    temp := head
+    for temp != nil {
+        count++
+        temp = temp.Next
+    }
+    dummy := &ListNode{0, head}
+    prevGroupEnd := dummy
+
+    for count >= k {
+        curr := prevGroupEnd.Next
+        nextGroupStart := curr.Next
+        newHead := reverse(curr, nextGroupStart)
+        prevGroupEnd.Next = newHead
+        curr.Next = nextGroupStart
+        prevGroupEnd = curr
+        count -= k
+    }
+    return dummy.Next
+}
