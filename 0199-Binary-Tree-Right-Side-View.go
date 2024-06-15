25b4960879e0bdec3ba2d75eb61e38d2586e0579
Bansari Thummar <bansarithummar@gmail.com>
2024-06-14 21:13:29 -0500

    Update 0199-Binary-Tree-Right-Side-View.go

--- 0199-Binary-Tree-Right-Side-View.go
+++ 0199-Binary-Tree-Right-Side-View.go
@@ -1,1 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 
+func rightSideView(root *TreeNode) []int {
+    if root == nil {
+		return []int{}
+	}
+	var result []int
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		var rightmostNode int
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			rightmostNode = node.Val
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		result = append(result, rightmostNode)
+	}
+	return result    
+}
