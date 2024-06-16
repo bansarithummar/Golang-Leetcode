06b9bac1379b8eb2d74481a2abbd3c1aa53a45ea
Bansari Thummar <bansarithummar@gmail.com>
2024-06-15 20:43:08 -0500

    Update 1448-Count-Good-Nodes-in-Binary-Tree.go

--- 1448-Count-Good-Nodes-in-Binary-Tree.go
+++ 1448-Count-Good-Nodes-in-Binary-Tree.go
@@ -1,1 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 
+func goodNodes(root *TreeNode) int {
+    return dfs(root, root.Val)
+}
+
+func dfs(node *TreeNode, maxVal int) int {
+	if node == nil {
+		return 0
+	}
+	good := 0
+	if node.Val >= maxVal {
+		good = 1
+	}
+	newMaxVal := max(maxVal, node.Val)
+	return good + dfs(node.Left, newMaxVal) + dfs(node.Right, newMaxVal) 
+}
