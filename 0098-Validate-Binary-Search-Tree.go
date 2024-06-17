5ea4b2160f905c6f6a71684e9b1716cdfcf48e8a
Bansari Thummar <bansarithummar@gmail.com>
2024-06-16 20:16:36 -0500

    Update 0098-Validate-Binary-Search-Tree.go

--- 0098-Validate-Binary-Search-Tree.go
+++ 0098-Validate-Binary-Search-Tree.go
@@ -1,1 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 
+func isValidBST(root *TreeNode) bool {
+    return validate(root, nil, nil)
+}
+
+func validate(node *TreeNode, lower *int, upper *int) bool {
+	if node == nil {
+		return true
+	}
+	
+	val := node.Val
+	
+	if lower != nil && val <= *lower {
+		return false
+	}
+	if upper != nil && val >= *upper {
+		return false
+	}
+	
+	if !validate(node.Right, &val, upper) {
+		return false
+	}
+	if !validate(node.Left, lower, &val) {
+		return false
+	}
+	
+	return true
+}
