577f069bba7ab6138b23842e44e444544ab57bad
Bansari Thummar <bansarithummar@gmail.com>
2024-06-17 22:02:01 -0500

    Update 0230-Kth-Smallest-Element-in-a-BST.go

--- 0230-Kth-Smallest-Element-in-a-BST.go
+++ 0230-Kth-Smallest-Element-in-a-BST.go
@@ -1,1 +1,28 @@
-
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func kthSmallest(root *TreeNode, k int) int {
+    count := 0
+    result := 0
+    var inOrder func(node *TreeNode)
+    inOrder = func(node *TreeNode) {
+        if node == nil || count >= k {
+            return
+        }
+        inOrder(node.Left)
+        count++
+        if count == k {
+            result = node.Val
+            return
+        }
+        inOrder(node.Right)
+    }    
+    inOrder(root)
+    return result
+}
