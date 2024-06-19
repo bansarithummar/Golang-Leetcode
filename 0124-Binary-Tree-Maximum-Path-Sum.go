4c69ad78e2e62f84788aed34fb7b01e6a78e7269
Bansari Thummar <bansarithummar@gmail.com>
2024-06-19 16:08:59 -0500

    Update 0124-Binary-Tree-Maximum-Path-Sum.go

--- 0124-Binary-Tree-Maximum-Path-Sum.go
+++ 0124-Binary-Tree-Maximum-Path-Sum.go
@@ -1,1 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+var maxSum int
+
+func maxPathSum(root *TreeNode) int {
+    maxSum = math.MinInt32
+    maxGain(root)
+    return maxSum
+}
+
+func maxGain(node *TreeNode) int {
+    if node == nil {
+        return 0
+    }
+    leftGain := max(maxGain(node.Left), 0)
+    rightGain := max(maxGain(node.Right), 0)
+    currentMaxPath := node.Val + leftGain + rightGain
+    maxSum = max(maxSum, currentMaxPath)
+    return node.Val + max(leftGain, rightGain)
+}
 
