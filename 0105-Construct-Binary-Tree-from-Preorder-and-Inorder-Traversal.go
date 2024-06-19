4a58d18867f89318342970649d4a4754cd321134
Bansari Thummar <bansarithummar@gmail.com>
2024-06-18 19:05:20 -0500

    Update 0105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.go

--- 0105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.go
+++ 0105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.go
@@ -1,1 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 
+func buildTree(preorder []int, inorder []int) *TreeNode {
+    if len(preorder) == 0 || len(inorder) == 0 {
+        return nil
+    }
+    rootVal := preorder[0]
+    root := &TreeNode{Val: rootVal}
+    var rootIndex int
+    for i, val := range inorder {
+        if val == rootVal {
+            rootIndex = i
+            break
+        }
+    }
+    leftInorder := inorder[:rootIndex]
+    rightInorder := inorder[rootIndex+1:]
+    leftPreorder := preorder[1 : 1+len(leftInorder)]
+    rightPreorder := preorder[1+len(leftInorder):]
+    root.Left = buildTree(leftPreorder, leftInorder)
+    root.Right = buildTree(rightPreorder, rightInorder)  
+    return root    
+}
