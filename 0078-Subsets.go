543f2524e59337068e196ebb690eab98e9ac3eff
Bansari Thummar <bansarithummar@gmail.com>
2024-06-23 12:49:47 -0500

    Update 0078-Subsets.go

--- 0078-Subsets.go
+++ 0078-Subsets.go
@@ -1,1 +1,17 @@
-78. Subsets
+func subsets(nums []int) [][]int {
+    var result [][]int
+    var current []int
+    backtrack(nums, 0, current, &result)
+    return result
+}
+
+func backtrack(nums []int, start int, current []int, result *[][]int) {
+    subset := make([]int, len(current))
+    copy(subset, current)
+    *result = append(*result, subset)
+    for i := start; i < len(nums); i++ {
+        current = append(current, nums[i])
+        backtrack(nums, i+1, current, result)
+        current = current[:len(current)-1] 
+    }    
+}
