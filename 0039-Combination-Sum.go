78c90387a36b3d1e886989f2ab751d285ec8d020
Bansari Thummar <bansarithummar@gmail.com>
2024-06-24 16:48:51 -0500

    Update 0039-Combination-Sum.go

--- 0039-Combination-Sum.go
+++ 0039-Combination-Sum.go
@@ -1,1 +1,24 @@
+func combinationSum(candidates []int, target int) [][]int {
+    var result [][]int
+    var current []int
+    backtrack(candidates, target, 0, current, &result)
+    return result
+}
 
+
+func backtrack(candidates []int, target int, start int, current []int, result *[][]int) {
+    if target == 0 {
+        combination := make([]int, len(current))
+        copy(combination, current)
+        *result = append(*result, combination)
+        return
+    }
+    
+    for i := start; i < len(candidates); i++ {
+        if candidates[i] <= target {
+            current = append(current, candidates[i])
+            backtrack(candidates, target - candidates[i], i, current, result)
+            current = current[:len(current)-1]
+        }
+    }
+}
