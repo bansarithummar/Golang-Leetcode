678d102d7ea9ed7e7154729a1412563fa43ec3e9
Bansari Thummar <bansarithummar@gmail.com>
2024-06-25 20:29:50 -0500

    Update 0046-Permutations.go

--- 0046-Permutations.go
+++ 0046-Permutations.go
@@ -1,1 +1,21 @@
+func permute(nums []int) [][]int {
+    var result [][]int
+    backtrack(nums, 0, &result)
+    return result
+}
 
+
+func backtrack(nums []int, start int, result *[][]int) {
+    if start == len(nums) {
+        permutation := make([]int, len(nums))
+        copy(permutation, nums)
+        *result = append(*result, permutation)
+        return
+    }
+    
+    for i := start; i < len(nums); i++ {
+        nums[start], nums[i] = nums[i], nums[start]
+        backtrack(nums, start+1, result)
+        nums[start], nums[i] = nums[i], nums[start]
+    }    
+}
