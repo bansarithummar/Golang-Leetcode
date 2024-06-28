c2cad03bbfdd5061ae53a56e43e77fcca6a40b24
Bansari Thummar <bansarithummar@gmail.com>
2024-06-27 21:08:31 -0500

    Update 0040-Combination-Sum-II.go

--- 0040-Combination-Sum-II.go
+++ 0040-Combination-Sum-II.go
@@ -1,1 +1,28 @@
+func combinationSum2(candidates []int, target int) [][]int {
+    var result [][]int
+    var current []int
+    sort.Ints(candidates)
+    backtrack(candidates, target, 0, current, &result)
+    return result
+}
 
+func backtrack(candidates []int, target int, start int, current []int, result *[][]int) {
+    if target == 0 {
+        combination := make([]int, len(current))
+        copy(combination, current)
+        *result = append(*result, combination)
+        return
+    }
+
+    for i := start; i < len(candidates); i++ {
+        if i > start && candidates[i] == candidates[i-1] {
+            continue
+        }
+        if candidates[i] > target {
+            break
+        }
+        current = append(current, candidates[i])
+        backtrack(candidates, target-candidates[i], i+1, current, result)
+        current = current[:len(current)-1]
+    }    
+}
