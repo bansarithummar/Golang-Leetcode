0a35489cb1bfcc9fa43e6095fd51e21823917385
Bansari Thummar <bansarithummar@gmail.com>
2024-07-03 20:43:44 -0500

    Update 1046-Last-Stone-Weight.go

--- 1046-Last-Stone-Weight.go
+++ 1046-Last-Stone-Weight.go
@@ -1,1 +1,36 @@
+type MaxHeap []int
 
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func lastStoneWeight(stones []int) int {
+    h := &MaxHeap{}
+	heap.Init(h)
+	for _, stone := range stones {
+		heap.Push(h, stone)
+	}
+	for h.Len() > 1 {
+		y := heap.Pop(h).(int)
+		x := heap.Pop(h).(int)
+		if x != y {
+			heap.Push(h, y-x)
+		}
+	}
+	if h.Len() == 0 {
+		return 0
+	}
+	return heap.Pop(h).(int)    
+}
