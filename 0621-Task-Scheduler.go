a4d3cf91ac143e4cb1ed79ac9b1d82aef57dfacc
Bansari Thummar <bansarithummar@gmail.com>
2024-07-07 18:28:19 -0500

    Update 0621-Task-Scheduler.go

--- 0621-Task-Scheduler.go
+++ 0621-Task-Scheduler.go
@@ -1,1 +1,30 @@
+func leastInterval(tasks []byte, n int) int {
+    taskCount := make(map[byte]int)
+    maxFreq := 0
+    for _, task := range tasks {
+        taskCount[task]++
+        if taskCount[task] > maxFreq {
+            maxFreq = taskCount[task]
+        }
+    }
+    maxCount := 0
+    for _, count := range taskCount {
+        if count == maxFreq {
+            maxCount++
+        }
+    }
+    partCount := maxFreq - 1
+    partLength := n - (maxCount - 1)
+    emptySlots := partCount * partLength
+    availableTasks := len(tasks) - maxFreq*maxCount
+    idles := max(0, emptySlots - availableTasks)
 
+    return len(tasks) + idles
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b    
+}
