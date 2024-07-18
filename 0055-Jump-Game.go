c38d91ac18d756a9139cd07b06b3a3d40dbdcbd8
Bansari Thummar <bansarithummar@gmail.com>
2024-07-18 16:01:21 -0500

    Update 0055-Jump-Game.go

--- 0055-Jump-Game.go
+++ 0055-Jump-Game.go
@@ -1,1 +1,20 @@
+func canJump(nums []int) bool {
+    furthest := 0
+    for i := 0; i < len(nums); i++ {
+        if i > furthest {
+            return false
+        }
+        furthest = max(furthest, i + nums[i])
+        if furthest >= len(nums) - 1 {
+            return true
+        }
+    }
+    return false
+}
 
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b   
+}
