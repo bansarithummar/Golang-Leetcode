9cca4ff6e96e6c4842712a30baa9bf225ab96627
Bansari Thummar <bansarithummar@gmail.com>
2024-07-20 12:36:49 -0500

    Update 0134-Gas-Station.go

--- 0134-Gas-Station.go
+++ 0134-Gas-Station.go
@@ -1,1 +1,17 @@
-
+func canCompleteCircuit(gas []int, cost []int) int {
+    total_gas := 0
+    current_gas := 0
+    start_index := 0
+    for i := 0; i < len(gas); i++ {
+        total_gas += gas[i] - cost[i]
+        current_gas += gas[i] - cost[i]
+        if current_gas < 0 {
+            start_index = i + 1
+            current_gas = 0
+        }
+    }
+    if total_gas < 0 {
+        return -1
+    }
+    return start_index
+}
