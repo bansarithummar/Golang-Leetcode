b5c53e26d51d33960b7575af40582013827d0793
Bansari Thummar <bansarithummar@gmail.com>
2024-07-20 12:34:16 -0500

    Update 0045-Jump-Game-II.go

--- 0045-Jump-Game-II.go
+++ 0045-Jump-Game-II.go
@@ -1,1 +1,18 @@
-
+func jump(nums []int) int {
+    jumps := 0
+    current_end := 0
+    furthest := 0
+    for i := 0; i < len(nums) - 1; i++ {
+        if i + nums[i] > furthest {
+            furthest = i + nums[i]
+        }
+        if i == current_end {
+            jumps++
+            current_end = furthest
+            if current_end >= len(nums) - 1 {
+                break
+            }
+        }
+    }
+    return jumps    
+}
