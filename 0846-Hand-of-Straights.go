4b741c5a4f6c206baeeb7c6214b8241d24c672ba
Bansari Thummar <bansarithummar@gmail.com>
2024-07-22 12:32:22 -0500

    Update 0846-Hand-of-Straights.go

--- 0846-Hand-of-Straights.go
+++ 0846-Hand-of-Straights.go
@@ -1,1 +1,21 @@
-
+func isNStraightHand(hand []int, groupSize int) bool {
+    if len(hand) % groupSize != 0 {
+        return false
+    }
+    cardCount := make(map[int]int)
+    for _, card := range hand {
+        cardCount[card]++
+    }
+    sort.Ints(hand)
+    for _, card := range hand {
+        if cardCount[card] > 0 {
+            for i := 0; i < groupSize; i++ {
+                if cardCount[card + i] == 0 {
+                    return false
+                }
+                cardCount[card + i]--
+            }
+        }
+    }
+    return true
+}
