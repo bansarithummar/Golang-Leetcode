4a90e9d350b69e555f3c28065a4d3c1ebadd21e5
Bansari Thummar <bansarithummar@gmail.com>
2024-07-23 20:02:35 -0500

    Update 0763-Partition-Labels.go

--- 0763-Partition-Labels.go
+++ 0763-Partition-Labels.go
@@ -1,1 +1,18 @@
-
+func partitionLabels(s string) []int {
+    lastOccurrence := make(map[rune]int)
+    for i, char := range s {
+        lastOccurrence[char] = i
+    }    
+    var partitions []int
+    start, end := 0, 0
+    for i, char := range s {
+        if lastOccurrence[char] > end {
+            end = lastOccurrence[char]
+        }
+        if i == end {
+            partitions = append(partitions, end - start + 1)
+            start = i + 1
+        }
+    }   
+    return partitions   
+}
