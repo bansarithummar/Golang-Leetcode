b3b11662144c6b1aa3419d10c217213d7c567b86
Bansari Thummar <bansarithummar@gmail.com>
2024-08-05 13:24:59 -0500

    Update 0130-Surrounded-Regions.go

--- 0130-Surrounded-Regions.go
+++ 0130-Surrounded-Regions.go
@@ -1,1 +1,36 @@
+func solve(board [][]byte)  {
+    if len(board) == 0 {
+		return
+	}
+	m, n := len(board), len(board[0])
+	var dfs func(int, int)
+	dfs = func(x, y int) {
+		if x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O' {
+			return
+		}
+		board[x][y] = '#'
+		directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+		for _, d := range directions {
+			dfs(x+d[0], y+d[1])
+		}
+	}
 
+	for i := 0; i < m; i++ {
+		dfs(i, 0)
+		dfs(i, n-1)
+	}
+	for j := 0; j < n; j++ {
+		dfs(0, j)
+		dfs(m-1, j)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			} else if board[i][j] == '#' {
+				board[i][j] = 'O'
+			}
+		}
+	}   
+}
