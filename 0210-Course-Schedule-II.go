34c8928391720ffaff42d8928cb36b0891df6a35
Bansari Thummar <bansarithummar@gmail.com>
2024-08-07 22:10:25 -0500

    Update 0210-Course-Schedule-II.go

--- 0210-Course-Schedule-II.go
+++ 0210-Course-Schedule-II.go
@@ -1,1 +1,31 @@
-
+func findOrder(numCourses int, prerequisites [][]int) []int {
+    graph := make([][]int, numCourses)
+    inDegree := make([]int, numCourses)
+    for _, pair := range prerequisites {
+        course, prereq := pair[0], pair[1]
+        graph[prereq] = append(graph[prereq], course)
+        inDegree[course]++
+    }
+    queue := []int{}
+    for i := 0; i < numCourses; i++ {
+        if inDegree[i] == 0 {
+            queue = append(queue, i)
+        }
+    }   
+    order := []int{}
+    for len(queue) > 0 {
+        current := queue[0]
+        queue = queue[1:]
+        order = append(order, current)
+        for _, next := range graph[current] {
+            inDegree[next]--
+            if inDegree[next] == 0 {
+                queue = append(queue, next)
+            }
+        }
+    }
+    if len(order) == numCourses {
+        return order
+    }
+    return []int{}   
+}
