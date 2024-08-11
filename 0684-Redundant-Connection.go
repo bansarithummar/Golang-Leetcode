9aa4462757d32003f6ab29d0e786c73cbaad0c75
Bansari Thummar <bansarithummar@gmail.com>
2024-08-10 23:14:39 -0500

    Update 0684-Redundant-Connection.go

--- 0684-Redundant-Connection.go
+++ 0684-Redundant-Connection.go
@@ -1,1 +1,56 @@
+type UnionFind struct {
+    parent []int
+    rank   []int
+}
 
+func NewUnionFind(size int) *UnionFind {
+    parent := make([]int, size)
+    rank := make([]int, size)
+    for i := 0; i < size; i++ {
+        parent[i] = i
+        rank[i] = 1
+    }
+    return &UnionFind{parent: parent, rank: rank}
+}
+
+func (uf *UnionFind) Find(x int) int {
+    if uf.parent[x] != x {
+        uf.parent[x] = uf.Find(uf.parent[x])
+    }
+    return uf.parent[x]
+}
+
+// Union by rank
+func (uf *UnionFind) Union(x, y int) bool {
+    rootX := uf.Find(x)
+    rootY := uf.Find(y)
+
+    if rootX == rootY {
+        return false // They are already connected, meaning this edge is redundant.
+    }
+
+    // Union by rank
+    if uf.rank[rootX] > uf.rank[rootY] {
+        uf.parent[rootY] = rootX
+    } else if uf.rank[rootX] < uf.rank[rootY] {
+        uf.parent[rootX] = rootY
+    } else {
+        uf.parent[rootY] = rootX
+        uf.rank[rootX]++
+    }
+    
+    return true
+}
+
+// Function to find the redundant connection
+func findRedundantConnection(edges [][]int) []int {
+    uf := NewUnionFind(len(edges) + 1) // +1 because nodes are 1-indexed
+    
+    for _, edge := range edges {
+        if !uf.Union(edge[0]-1, edge[1]-1) { // Subtract 1 to make the nodes 0-indexed
+            return edge
+        }
+    }
+    
+    return nil // Should not reach here
+}
