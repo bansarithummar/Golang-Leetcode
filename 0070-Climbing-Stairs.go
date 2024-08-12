5ec05067572fbfadc6f284c46c3716e8f0275f77
Bansari Thummar <bansarithummar@gmail.com>
2024-08-12 13:52:51 -0500

    Update 0070-Climbing-Stairs.go

--- 0070-Climbing-Stairs.go
+++ 0070-Climbing-Stairs.go
@@ -1,1 +1,15 @@
+func climbStairs(n int) int {
+    if n <= 2 {
+        return n
+    }
 
+    first, second := 1, 2
+
+    for i := 3; i <= n; i++ {
+        next := first + second
+        first = second
+        second = next
+    }
+
+    return second
+}
