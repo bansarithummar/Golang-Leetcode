0e1c9a9ede7648c98a35e4df9d96226ab405d5c5
Bansari Thummar <bansarithummar@gmail.com>
2024-08-15 15:04:59 -0500

    Update 0213-House-Robber-II.go

--- 0213-House-Robber-II.go
+++ 0213-House-Robber-II.go
@@ -1,1 +1,38 @@
+func rob(nums []int) int {
+    n := len(nums)
+    if n == 0 {
+        return 0
+    }
+    if n == 1 {
+        return nums[0]
+    }
+    var robLinear func(nums []int) int
+    robLinear = func(nums []int) int {
+        n := len(nums)
+        if n == 0 {
+            return 0
+        }
+        if n == 1 {
+            return nums[0]
+        }
+        dp := make([]int, n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i := 2; i < n; i++ {
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+        }
+        
+        return dp[n-1]
+    }
+    robFirst := robLinear(nums[:n-1])
+    robLast := robLinear(nums[1:])
+    return max(robFirst, robLast)
+}
 
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b    
+}
