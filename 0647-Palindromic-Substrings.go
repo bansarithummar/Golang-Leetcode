c5d2b2bf6e8370c91e1d36a7c2dca620fc630a10
Bansari Thummar <bansarithummar@gmail.com>
2024-08-17 09:42:11 -0500

    Update 0647-Palindromic-Substrings.go

--- 0647-Palindromic-Substrings.go
+++ 0647-Palindromic-Substrings.go
@@ -1,1 +1,18 @@
+func countSubstrings(s string) int {
+    count := 0
+    for i := 0; i < len(s); i++ {
+        count += expandAroundCenter(s, i, i)
+        count += expandAroundCenter(s, i, i+1)
+    }
+    return count
+}
 
+func expandAroundCenter(s string, left int, right int) int {
+    count := 0
+    for left >= 0 && right < len(s) && s[left] == s[right] {
+        count++
+        left--
+        right++
+    }
+    return count   
+}
