db17a46af9ab5531680f0cf8b586070cd0c175ed
Bansari Thummar <bansarithummar@gmail.com>
2024-08-19 17:36:53 -0500

    Update 0322-Coin-Change.go

--- 0322-Coin-Change.go
+++ 0322-Coin-Change.go
@@ -1,1 +1,19 @@
-
+func coinChange(coins []int, amount int) int {
+    dp := make([]int, amount+1)
+	for i := range dp {
+		dp[i] = amount + 1
+	}
+	dp[0] = 0
+	
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if coin <= i {
+				dp[i] = int(math.Min(float64(dp[i]), float64(dp[i-coin]+1)))
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}	
+	return dp[amount]   
+}
