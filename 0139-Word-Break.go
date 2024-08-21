c847f49080c6b4f6c738e4699d133d32e0a12422
Bansari Thummar <bansarithummar@gmail.com>
2024-08-21 18:37:25 -0500

    Update 0139-Word-Break.go

--- 0139-Word-Break.go
+++ 0139-Word-Break.go
@@ -1,1 +1,19 @@
+func wordBreak(s string, wordDict []string) bool {
+    dp := make([]bool, len(s)+1)
+	dp[0] = true 
 
+	wordSet := make(map[string]bool)
+	for _, word := range wordDict {
+		wordSet[word] = true
+	}
+
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && wordSet[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]   
+}
