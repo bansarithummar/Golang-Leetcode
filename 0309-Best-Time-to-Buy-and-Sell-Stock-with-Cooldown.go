3a1938d475182e6f7fa5496511d17aa3ee507430
Bansari Thummar <bansarithummar@gmail.com>
2024-08-26 14:45:40 -0500

    Update 0309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.go

--- 0309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.go
+++ 0309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.go
@@ -1,1 +1,26 @@
+func maxProfit(prices []int) int {
+    n := len(prices)
+    if n == 0 {
+        return 0
+    }
+    
+    hold, sold, cooldown := -prices[0], 0, 0
+    
+    for i := 1; i < n; i++ {
+        newHold := max(hold, cooldown - prices[i])
+        newSold := hold + prices[i]
+        newCooldown := max(cooldown, sold)
+        
+        hold = newHold
+        sold = newSold
+        cooldown = newCooldown
+    }
+    return max(cooldown, sold)
+}
 
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
