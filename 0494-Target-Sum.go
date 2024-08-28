97277d07d08a8340b998f4529f462951353868bd
Bansari Thummar <bansarithummar@gmail.com>
2024-08-28 15:26:12 -0500

    Update 0494-Target-Sum.go

--- 0494-Target-Sum.go
+++ 0494-Target-Sum.go
@@ -1,1 +1,20 @@
+func findTargetSumWays(nums []int, target int) int {
+    totalSum := 0
+    for _, num := range nums {
+        totalSum += num
+    }
+    if (target + totalSum) % 2 != 0 || (target + totalSum) < 0 {
+        return 0
+    }
 
+    S1 := (target + totalSum) / 2
+    dp := make([]int, S1 + 1)
+    dp[0] = 1 
+
+    for _, num := range nums {
+        for i := S1; i >= num; i-- {
+            dp[i] += dp[i - num]
+        }
+    }
+    return dp[S1]    
+}
