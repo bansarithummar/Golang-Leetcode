8db31e1461aad27ababdaf6bb461f93d291982a9
Bansari Thummar <bansarithummar@gmail.com>
2024-09-21 12:06:44 -0500

    Update 0371-Sum-of-Two-Integers.go

--- 0371-Sum-of-Two-Integers.go
+++ 0371-Sum-of-Two-Integers.go
@@ -1,10 +1,7 @@
 func getSum(a int, b int) int {
     for b != 0 {
-        // Calculate the carry
         carry := (a & b) << 1
-        // Calculate the sum without carry
         a = a ^ b
-        // Update b to be the carry
         b = carry
     }
     return a
