1e2c5beec2afb52ad5a946158a969fe8ae245b2d
Bansari Thummar <bansarithummar@gmail.com>
2024-10-25 14:36:28 -0500

    Update 0048-Rotate-Image.go

--- 0048-Rotate-Image.go
+++ 0048-Rotate-Image.go
@@ -1,1 +1,17 @@
+func rotate(matrix [][]int)  {
+    n := len(matrix)
 
+    // Step 1: Transpose the matrix
+    for i := 0; i < n; i++ {
+        for j := i + 1; j < n; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+    }
+
+    for i := 0; i < n; i++ {
+        for j := 0; j < n/2; j++ {
+            matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+        }
+    }
+    
+}
