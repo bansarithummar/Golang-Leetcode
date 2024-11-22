7e2a41754f4d4bd197578e8fe9024e408fb185c5
Bansari Thummar <bansarithummar@gmail.com>
2024-11-21 19:27:47 -0600

    Update 0202-Happy-Number.go

--- 0202-Happy-Number.go
+++ 0202-Happy-Number.go
@@ -1,1 +1,21 @@
+func isHappy(n int) bool {
+    seen := make(map[int]bool)
 
+    sumOfSquares := func(num int) int {
+        sum := 0
+        for num > 0 {
+            digit := num % 10
+            sum += digit * digit
+            num /= 10
+        }
+        return sum
+    }
+
+    for n != 1 && !seen[n] {
+        seen[n] = true
+        n = sumOfSquares(n)
+    }
+
+    return n == 1
+    
+}
