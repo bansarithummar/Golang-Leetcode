4f28ab970ba6859491be7ac1d6b5ed380de28c97
Bansari Thummar <bansarithummar@gmail.com>
2025-01-11 20:45:36 -0600

    Update 2116-Check-if-a-Parentheses-String-Can-Be-Valid.go

--- 2116-Check-if-a-Parentheses-String-Can-Be-Valid.go
+++ 2116-Check-if-a-Parentheses-String-Can-Be-Valid.go
@@ -1,11 +1,9 @@
 func canBeValid(s string, locked string) bool {
     n := len(s)
-    // If the length of the string is odd, it cannot be valid
     if n%2 != 0 {
         return false
     }
 
-    // Check from left to right
     openCount, unlockCount := 0, 0
     for i := 0; i < n; i++ {
         if locked[i] == '0' {
@@ -23,7 +21,6 @@ func canBeValid(s string, locked string) bool {
         }
     }
 
-    // Check from right to left
     closeCount, unlockCount := 0, 0
     for i := n - 1; i >= 0; i-- {
         if locked[i] == '0' {
