11f48f965cbcabe42b6cc5489ede9967155f810e
Bansari Thummar <bansarithummar@gmail.com>
2025-01-11 20:45:02 -0600

    Update 2116-Check-if-a-Parentheses-String-Can-Be-Valid.go

--- 2116-Check-if-a-Parentheses-String-Can-Be-Valid.go
+++ 2116-Check-if-a-Parentheses-String-Can-Be-Valid.go
@@ -1,1 +1,45 @@
+func canBeValid(s string, locked string) bool {
+    n := len(s)
+    // If the length of the string is odd, it cannot be valid
+    if n%2 != 0 {
+        return false
+    }
 
+    // Check from left to right
+    openCount, unlockCount := 0, 0
+    for i := 0; i < n; i++ {
+        if locked[i] == '0' {
+            unlockCount++
+        } else if s[i] == '(' {
+            openCount++
+        } else { // s[i] == ')' and locked[i] == '1'
+            if openCount > 0 {
+                openCount--
+            } else if unlockCount > 0 {
+                unlockCount--
+            } else {
+                return false
+            }
+        }
+    }
+
+    // Check from right to left
+    closeCount, unlockCount := 0, 0
+    for i := n - 1; i >= 0; i-- {
+        if locked[i] == '0' {
+            unlockCount++
+        } else if s[i] == ')' {
+            closeCount++
+        } else { // s[i] == '(' and locked[i] == '1'
+            if closeCount > 0 {
+                closeCount--
+            } else if unlockCount > 0 {
+                unlockCount--
+            } else {
+                return false
+            }
+        }
+    }
+
+    return true
+}
