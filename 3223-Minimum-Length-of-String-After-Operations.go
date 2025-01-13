948ae7ad16e98ad4dcca6a8ed35b00a2e2bd6e09
Bansari Thummar <bansarithummar@gmail.com>
2025-01-12 20:26:41 -0600

    Update 3223-Minimum-Length-of-String-After-Operations.go

--- 3223-Minimum-Length-of-String-After-Operations.go
+++ 3223-Minimum-Length-of-String-After-Operations.go
@@ -1,1 +1,21 @@
+func minimumLength(s string) int {
+    freqMap := make(map[rune]int)
 
+    for _, c := range s {
+        freqMap[c]++
+    }
+
+    deletions := 0
+
+    for _, count := range freqMap {
+        if count > 2 {
+            if count%2 == 0 {
+                deletions += (count - 2)
+            } else {
+                deletions += (count - 1)
+            }
+        }
+    }
+
+    return len(s) - deletions  
+}
