c221baf4e310e380920bf44424acfbf420e9088e
Bansari Thummar <bansarithummar@gmail.com>
2025-01-13 20:30:44 -0600

    Update 2657-Find-the-Prefix-Common-Array-of-Two-Arrays.go

--- 2657-Find-the-Prefix-Common-Array-of-Two-Arrays.go
+++ 2657-Find-the-Prefix-Common-Array-of-Two-Arrays.go
@@ -1,1 +1,23 @@
+func findThePrefixCommonArray(A []int, B []int) []int {
+    n := len(A)
+    result := make([]int, n)
+    seen := make(map[int]bool) // Tracks numbers seen so far in A and B
+    common := 0               // Tracks the count of common elements
 
+    for i := 0; i < n; i++ {
+        if seen[A[i]] {
+            common++
+        } else {
+            seen[A[i]] = true
+        }
+        if seen[B[i]] {
+            common++
+        } else {
+            seen[B[i]] = true
+        }
+        result[i] = common
+    }
+
+    return result
+    
+}
