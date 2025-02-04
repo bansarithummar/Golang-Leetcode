6ef354418d8f42bfcae8ed16a0275227d7814d54
Bansari Thummar <bansarithummar@gmail.com>
2025-02-03 21:34:35 -0600

    Update 1800-Maximum-Ascending-Subarray-Sum.go

--- 1800-Maximum-Ascending-Subarray-Sum.go
+++ 1800-Maximum-Ascending-Subarray-Sum.go
@@ -1,1 +1,16 @@
+func maxAscendingSum(nums []int) int {
+    maxSum, currSum := nums[0], nums[0]
+    
+    for i := 1; i < len(nums); i++ {
+        if nums[i] > nums[i-1] {
+            currSum += nums[i]
+        } else {
+            currSum = nums[i]
+        }
+        if currSum > maxSum {
+            maxSum = currSum
+        }
+    }    
+    return maxSum   
+}
 
