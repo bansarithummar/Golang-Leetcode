f55868e6907506c6d3c39b1aa607a059a95136ff
Bansari Thummar <bansarithummar@gmail.com>
2025-02-05 17:30:17 -0600

    Update 1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.go

--- 1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.go
+++ 1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.go
@@ -1,1 +1,18 @@
+func areAlmostEqual(s1 string, s2 string) bool {
+    if s1 == s2 {
+        return true
+    }
 
+    var diff []int
+    for i := range s1 {
+        if s1[i] != s2[i] {
+            diff = append(diff, i)
+        }
+        if len(diff) > 2 {
+            return false
+        }
+    }
+
+    return len(diff) == 2 && s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]]
+}
+    
