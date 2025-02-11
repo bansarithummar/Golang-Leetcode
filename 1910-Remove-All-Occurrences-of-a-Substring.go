27cf15c752a92a5b953e4fd724a262c2e9f8861c
Bansari Thummar <bansarithummar@gmail.com>
2025-02-11 14:01:45 -0600

    Update 1910-Remove-All-Occurrences-of-a-Substring.go

--- 1910-Remove-All-Occurrences-of-a-Substring.go
+++ 1910-Remove-All-Occurrences-of-a-Substring.go
@@ -1,1 +1,23 @@
+func removeOccurrences(s string, part string) string {
+    // Edge cases
+    if len(s) == 0 || len(part) == 0 {
+        return s
+    }
+    var builder strings.Builder
 
+    for i := 0; i < len(s); i++ {
+        builder.WriteByte(s[i])
+
+        currLen := builder.Len()
+        if currLen >= len(part) {
+
+            curr := builder.String()
+            if curr[currLen-len(part):] == part {
+                // Remove part by resetting builder length
+                builder.Reset()
+                builder.WriteString(curr[:currLen-len(part)])
+            }
+        }
+    }    
+    return builder.String()
+}
