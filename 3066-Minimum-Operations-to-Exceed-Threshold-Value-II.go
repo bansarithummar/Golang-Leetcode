4015fd834b261657a4af32ac4f88d573544df86a
Bansari Thummar <bansarithummar@gmail.com>
2025-02-12 20:14:56 -0600

    Update 3066-Minimum-Operations-to-Exceed-Threshold-Value-II.go

--- 3066-Minimum-Operations-to-Exceed-Threshold-Value-II.go
+++ 3066-Minimum-Operations-to-Exceed-Threshold-Value-II.go
@@ -1,21 +1,18 @@
 func minOperations(nums []int, k int) int {
     h := &hp{} 
    heap.Init(h)
-   
-   // Add all nums to heap
+  
    for _, num := range nums {
        heap.Push(h, num)
    }
    
    operations := 0
-   
-   // Process until min element >= k
+
    for h.Len() >= 2 && h.IntSlice[0] < k {
        // Get two smallest elements
        x := heap.Pop(h).(int)
        y := heap.Pop(h).(int)
-       
-       // Calculate new element and add back
+   
        newVal := min(x,y)*2 + max(x,y)
        heap.Push(h, newVal)
        operations++
