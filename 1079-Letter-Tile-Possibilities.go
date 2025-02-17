5178cede49ed90d7dc561cfcea0284e39d860212
Bansari Thummar <bansarithummar@gmail.com>
2025-02-17 16:31:49 -0600

    Update 1079-Letter-Tile-Possibilities.go

--- 1079-Letter-Tile-Possibilities.go
+++ 1079-Letter-Tile-Possibilities.go
@@ -1,1 +1,32 @@
-
+func numTilePossibilities(tiles string) int {
+   // Build frequency map
+   freq := make(map[rune]int)
+   for _, t := range tiles {
+       freq[t]++
+   }
+   
+   // Backtrack to generate sequences
+   var dfs func() int
+   dfs = func() int {
+       count := 0
+       
+       // Try each available character
+       for c, f := range freq {
+           if f > 0 {
+               // Use this character
+               freq[c]--
+               count++
+               
+               // Recursively try remaining chars
+               count += dfs()
+               
+               // Backtrack
+               freq[c]++
+           }
+       }
+       
+       return count
+   }
+   
+   return dfs()
+}
