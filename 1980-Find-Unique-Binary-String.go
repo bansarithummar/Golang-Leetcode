6c6293adc3ff18d383dd1893056645ca95df4320
Bansari Thummar <bansarithummar@gmail.com>
2025-02-19 18:57:54 -0600

    Update 1980-Find-Unique-Binary-String.go

--- 1980-Find-Unique-Binary-String.go
+++ 1980-Find-Unique-Binary-String.go
@@ -1,1 +1,16 @@
-
+func findDifferentBinaryString(nums []string) string {
+    n := len(nums)
+   result := make([]byte, n)
+   
+   // Apply diagonal argument - flip each bit at position i
+   for i := 0; i < n; i++ {
+       if nums[i][i] == '0' {
+           result[i] = '1'
+       } else {
+           result[i] = '0'
+       }
+   }
+   
+   return string(result)
+    
+}
