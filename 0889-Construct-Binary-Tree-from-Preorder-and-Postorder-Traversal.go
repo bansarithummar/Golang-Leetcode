66b39bac67378757c8a598e3d9743f4491864c49
Bansari Thummar <bansarithummar@gmail.com>
2025-02-23 15:21:22 -0600

    Update 0889-Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.go

--- 0889-Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.go
+++ 0889-Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.go
@@ -1,19 +1,14 @@
-
 func constructFromPrePost(preorder []int, postorder []int) *TreeNode {
     if len(preorder) == 0 {
        return nil
    }
-   
-   // Create root from first preorder element
+
    root := &TreeNode{Val: preorder[0]}
-   
-   // Base case - single node
+
    if len(preorder) == 1 {
        return root
    }
    
-   // Find left subtree size
-   // Next element in preorder is left child
    leftVal := preorder[1]
    leftIdx := 0
    for i := 0; i < len(postorder); i++ {
@@ -22,8 +17,7 @@ func constructFromPrePost(preorder []int, postorder []int) *TreeNode {
            break
        }
    }
-   
-   // Recursively construct left and right subtrees
+
    root.Left = constructFromPrePost(
        preorder[1:leftIdx+1],
        postorder[:leftIdx],
@@ -35,5 +29,4 @@ func constructFromPrePost(preorder []int, postorder []int) *TreeNode {
    )
    
    return root
-    
 }
