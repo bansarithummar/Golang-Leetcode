b6ea0d399202c635d2f7543d309cda3ee1352796
Bansari Thummar <bansarithummar@gmail.com>
2025-02-25 20:32:21 -0600

    Update 1749-Maximum-Absolute-Sum-of-Any-Subarray.go

--- 1749-Maximum-Absolute-Sum-of-Any-Subarray.go
+++ 1749-Maximum-Absolute-Sum-of-Any-Subarray.go
@@ -4,18 +4,14 @@ func maxAbsoluteSum(nums []int) int {
     currentMax := 0
     currentMin := 0
     
-    // One-pass Kadane's algorithm with both max and min tracking
     for _, num := range nums {
-        // Update max subarray sum (for positive result)
         currentMax = max(currentMax+num, num)
         maxSum = max(maxSum, currentMax)
         
-        // Update min subarray sum (for negative result)
         currentMin = min(currentMin+num, num)
         minSum = min(minSum, currentMin)
     }
     
-    // Return the larger absolute value
     return max(maxSum, -minSum)
 }
 
@@ -30,6 +26,5 @@ func min(a, b int) int {
     if a < b {
         return a
     }
-    return b
-    
+    return b   
 }
