cc7f89abd34621d045c78e3379fdf3787ec7c843
Bansari Thummar <bansarithummar@gmail.com>
2025-03-19 10:54:45 -0500

    Update 3191-Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-I.go

--- 3191-Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-I.go
+++ 3191-Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-I.go
@@ -1,1 +1,22 @@
-
+func minOperations(nums []int) int {
+    n := len(nums)
+    count := 0
+    temp := make([]int, n)
+    copy(temp, nums)
+    
+    for i := 0; i < n-2; i++ {
+        if temp[i] == 0 {
+            temp[i] ^= 1
+            temp[i+1] ^= 1
+            temp[i+2] ^= 1
+            count++
+        }
+    }
+    
+    for i := 0; i < n; i++ {
+        if temp[i] == 0 {
+            return -1
+        }
+    }  
+    return count  
+}
