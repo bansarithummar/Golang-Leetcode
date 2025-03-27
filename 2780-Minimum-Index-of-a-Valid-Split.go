670c374251e7a5da71fc49f9c7324265e51bdd5e
Bansari Thummar <bansarithummar@gmail.com>
2025-03-27 10:01:32 -0500

    Update 2780-Minimum-Index-of-a-Valid-Split.go

--- 2780-Minimum-Index-of-a-Valid-Split.go
+++ 2780-Minimum-Index-of-a-Valid-Split.go
@@ -1,1 +1,44 @@
+func minimumIndex(nums []int) int {
+    dominant := findDominantElement(nums)
+    
+    totalCount := 0
+    for _, num := range nums {
+        if num == dominant {
+            totalCount++
+        }
+    }
 
+    leftCount, rightCount := 0, totalCount
+    
+    for i := 0; i < len(nums)-1; i++ {
+        if nums[i] == dominant {
+            leftCount++
+            rightCount--
+        }
+        
+        if leftCount * 2 > i+1 && rightCount * 2 > len(nums)-i-1 {
+            return i
+        }
+    }
+    
+    return -1
+}
+
+func findDominantElement(nums []int) int {
+    candidate, count := 0, 0
+    
+    for _, num := range nums {
+        if count == 0 {
+            candidate = num
+        }
+        
+        if num == candidate {
+            count++
+        } else {
+            count--
+        }
+    }
+    
+    return candidate
+    
+}
