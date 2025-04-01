b55bbec0052bed8d1a66cce7fff27f10126852de
Bansari Thummar <bansarithummar@gmail.com>
2025-03-31 20:31:58 -0500

    Update 2140-Solving-Questions-With-Brainpower.go

--- 2140-Solving-Questions-With-Brainpower.go
+++ 2140-Solving-Questions-With-Brainpower.go
@@ -2,15 +2,11 @@ func mostPoints(questions [][]int) int64 {
     n := len(questions)
     dp := make([]int64, n+1)
     
-    // Bottom-up DP approach
     for i := n - 1; i >= 0; i-- {
         points, jump := questions[i][0], questions[i][1]
-        
-        // Take maximum of solving current question or skipping it
         nextIndex := min(i+jump+1, n)
         dp[i] = max(int64(points) + dp[nextIndex], dp[i+1])
-    }
-    
+    } 
     return dp[0]
 }
 
@@ -25,6 +21,5 @@ func min(a, b int) int {
     if a < b {
         return a
     }
-    return b
-    
+    return b  
 }
