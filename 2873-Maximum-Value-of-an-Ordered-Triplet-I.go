080b93ef402df690760ac599ea5783d0180d9772
Bansari Thummar <bansarithummar@gmail.com>
2025-04-02 08:12:48 -0500

    Update 2873-Maximum-Value-of-an-Ordered-Triplet-I.go

--- 2873-Maximum-Value-of-an-Ordered-Triplet-I.go
+++ 2873-Maximum-Value-of-an-Ordered-Triplet-I.go
@@ -1,1 +1,36 @@
+func maximumTripletValue(nums []int) int64 {
+    n := len(nums)
+    
+    maxDiff := make([]int, n)
+    maxSoFar := nums[0]
+    
+    for j := 1; j < n-1; j++ {
+        // Update max difference for position j
+        maxDiff[j] = max(maxDiff[j-1], maxSoFar - nums[j])
+        // Update max element seen so far
+        maxSoFar = max(maxSoFar, nums[j])
+    }
+    
+    result := int64(0)
+    for k := 2; k < n; k++ {
+        // For each position k, find maximum (nums[i] - nums[j]) * nums[k]
+        value := int64(maxDiff[k-1] * nums[k])
+        result = max64(result, value)
+    }
+    
+    return result
+}
 
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func max64(a, b int64) int64 {
+    if a > b {
+        return a
+    }
+    return b   
+}
