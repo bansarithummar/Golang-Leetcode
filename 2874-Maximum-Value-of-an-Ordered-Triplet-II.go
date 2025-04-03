b6b4b118fbbffdf1fd6f61a08c3735e01eec5307
Bansari Thummar <bansarithummar@gmail.com>
2025-04-03 16:55:48 -0500

    Update 2874-Maximum-Value-of-an-Ordered-Triplet-II.go

--- 2874-Maximum-Value-of-an-Ordered-Triplet-II.go
+++ 2874-Maximum-Value-of-an-Ordered-Triplet-II.go
@@ -1,1 +1,23 @@
+func maximumTripletValue(nums []int) int64 {
+    maxNum := 0
+    maxDiff := 0
+    result := int64(0)
+    
+    for _, num := range nums {
+        curValue := int64(maxDiff * num)
+        
+        if curValue > result {
+            result = curValue
+        }
+        
+        if maxNum - num > maxDiff {
+            maxDiff = maxNum - num
+        }
 
+        if num > maxNum {
+            maxNum = num
+        }
+    }
+    
+    return result   
+}
