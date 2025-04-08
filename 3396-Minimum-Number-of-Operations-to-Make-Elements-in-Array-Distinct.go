9f438e1d52d3bb11ac9856c2dcb15358fe561e9a
Bansari Thummar <bansarithummar@gmail.com>
2025-04-07 19:46:58 -0500

    Update 3396-Minimum-Number-of-Operations-to-Make-Elements-in-Array-Distinct.go

--- 3396-Minimum-Number-of-Operations-to-Make-Elements-in-Array-Distinct.go
+++ 3396-Minimum-Number-of-Operations-to-Make-Elements-in-Array-Distinct.go
@@ -1,1 +1,30 @@
+func minimumOperations(nums []int) int {
+    n := len(nums)
+    
+    for ops := 0; ops <= (n+2)/3; ops++ {
+        remaining := nums
+        if 3*ops < n {
+            remaining = nums[3*ops:]
+        } else {
+            remaining = []int{}
+        }
+        
+        if hasDistinctElements(remaining) {
+            return ops
+        }
+    }
+    
+    return (n+2)/3
+  
+}
 
+func hasDistinctElements(nums []int) bool {
+    seen := make(map[int]bool)
+    for _, num := range nums {
+        if seen[num] {
+            return false
+        }
+        seen[num] = true
+    }
+    return true
+}
