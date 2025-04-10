6ddf6723ad4c4d11c192f94c2d48701fdcdf5500
Bansari Thummar <bansarithummar@gmail.com>
2025-04-09 22:17:41 -0500

    Update 2999-Count-the-Number-of-Powerful-Integers.go

--- 2999-Count-the-Number-of-Powerful-Integers.go
+++ 2999-Count-the-Number-of-Powerful-Integers.go
@@ -15,42 +15,36 @@ func numberOfPowerfulInt(start int64, finish int64, limit int, s string) int64 {
         
         memo := make(map[string]int64)
         
-        // Recursive DP function
         var dp func(int, bool) int64
         dp = func(pos int, tight bool) int64 {
-            // Base case: finished processing all positions
+            
             if pos == len(nStr) {
                 return 1
             }
             
-            // Memoization key
             key := fmt.Sprintf("%d_%t", pos, tight)
             if val, ok := memo[key]; ok {
                 return val
             }
             
-            // Check if we're in the suffix part
             inSuffix := pos >= len(nStr) - suffixLen
             var result int64 = 0
-            
-            // Determine upper bound for current position
+
             upperBound := limit
             if tight {
                 upperBound = min(upperBound, int(nStr[pos]-'0'))
             }
             
-            // If in suffix part, we must match the corresponding suffix digit
             if inSuffix {
                 suffixPos := pos - (len(nStr) - suffixLen)
                 targetDigit := int(s[suffixPos] - '0')
-                
-                // If the required suffix digit is valid, continue
+
                 if targetDigit <= upperBound {
                     newTight := tight && (targetDigit == int(nStr[pos]-'0'))
                     result += dp(pos+1, newTight)
                 }
             } else {
-                // If not in suffix, try all valid digits
+                
                 for digit := 0; digit <= upperBound; digit++ {
                     newTight := tight && (digit == int(nStr[pos]-'0'))
                     result += dp(pos+1, newTight)
@@ -63,8 +57,6 @@ func numberOfPowerfulInt(start int64, finish int64, limit int, s string) int64 {
         
         return dp(0, true)
     }
-    
-    // Result: count up to finish minus count up to (start-1)
     return countUpTo(finish) - countUpTo(start-1)
 }
 
