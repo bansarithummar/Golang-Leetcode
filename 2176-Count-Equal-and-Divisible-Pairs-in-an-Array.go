3d3f8b870b6ed5ea189565259800b13c39c2f306
Bansari Thummar <bansarithummar@gmail.com>
2025-04-17 15:26:45 -0500

    Update 2176-Count-Equal-and-Divisible-Pairs-in-an-Array.go

--- 2176-Count-Equal-and-Divisible-Pairs-in-an-Array.go
+++ 2176-Count-Equal-and-Divisible-Pairs-in-an-Array.go
@@ -1,18 +1,14 @@
-
 func countPairs(nums []int, k int) int {
     count := 0
     n := len(nums)
     
-    // Check all possible pairs (i,j) where i < j
     for i := 0; i < n; i++ {
         for j := i + 1; j < n; j++ {
-            // Check if nums[i] == nums[j] and (i*j) is divisible by k
             if nums[i] == nums[j] && (i*j)%k == 0 {
                 count++
             }
         }
     }
     
-    return count
-    
+    return count   
 }
