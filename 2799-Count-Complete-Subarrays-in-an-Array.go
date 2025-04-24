a2f0d005ab02eeb2e4cb17f357132fb5898b45ef
Bansari Thummar <bansarithummar@gmail.com>
2025-04-23 19:08:18 -0500

    Update 2799-Count-Complete-Subarrays-in-an-Array.go

--- 2799-Count-Complete-Subarrays-in-an-Array.go
+++ 2799-Count-Complete-Subarrays-in-an-Array.go
@@ -1,1 +1,26 @@
+func countCompleteSubarrays(nums []int) int {
+    distinctSet := make(map[int]bool)
+    for _, num := range nums {
+        distinctSet[num] = true
+    }
+    totalDistinct := len(distinctSet)
 
+    count := 0
+    n := len(nums)
+
+    for i := 0; i < n; i++ {
+        freq := make(map[int]int)
+        distinct := 0
+        for j := i; j < n; j++ {
+            if freq[nums[j]] == 0 {
+                distinct++
+            }
+            freq[nums[j]]++
+            if distinct == totalDistinct {
+                count++
+            }
+        }
+    }
+
+    return count
+}
