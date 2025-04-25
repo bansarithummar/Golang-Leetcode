19d084af05081f30246e260366087aaad42646b8
Bansari Thummar <bansarithummar@gmail.com>
2025-04-25 14:35:21 -0500

    Update 2845-Count-of-Interesting-Subarrays.go

--- 2845-Count-of-Interesting-Subarrays.go
+++ 2845-Count-of-Interesting-Subarrays.go
@@ -1,1 +1,19 @@
+func countInterestingSubarrays(nums []int, modulo int, k int) int64 {
+    count := int64(0)
+    prefixModCount := map[int]int{}
+    prefixModCount[0] = 1 // base case: prefix sum = 0
 
+    curr := 0
+    for _, num := range nums {
+        if num%modulo == k {
+            curr++
+        }
+        currMod := curr % modulo
+        want := (currMod - k + modulo) % modulo
+        count += int64(prefixModCount[want])
+        prefixModCount[currMod]++
+    }
+
+    return count
+    
+}
