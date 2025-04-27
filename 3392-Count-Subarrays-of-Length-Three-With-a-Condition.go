4a1ac8a6def550e114597c2367af65edccc5c659
Bansari Thummar <bansarithummar@gmail.com>
2025-04-27 11:08:16 -0500

    Update 3392-Count-Subarrays-of-Length-Three-With-a-Condition.go

--- 3392-Count-Subarrays-of-Length-Three-With-a-Condition.go
+++ 3392-Count-Subarrays-of-Length-Three-With-a-Condition.go
@@ -1,1 +1,10 @@
-
+func countSubarrays(nums []int) int {
+    count := 0
+    for i := 0; i+2 < len(nums); i++ {
+        mid := nums[i+1]
+        if mid%2 == 0 && nums[i]+nums[i+2] == mid/2 {
+            count++
+        }
+    }
+    return count
+}
