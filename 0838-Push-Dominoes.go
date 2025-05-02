d9efb2c851a6759fbaf5d73f53e04b3ac3b1884d
Bansari Thummar <bansarithummar@gmail.com>
2025-05-01 22:30:13 -0500

    Update 0838-Push-Dominoes.go

--- 0838-Push-Dominoes.go
+++ 0838-Push-Dominoes.go
@@ -2,7 +2,6 @@ func pushDominoes(dominoes string) string {
     n := len(dominoes)
     forces := make([]int, n)
     
-    // Apply force from 'R'
     force := 0
     for i := 0; i < n; i++ {
         if dominoes[i] == 'R' {
@@ -15,7 +14,6 @@ func pushDominoes(dominoes string) string {
         forces[i] += force
     }
 
-    // Apply force from 'L'
     force = 0
     for i := n - 1; i >= 0; i-- {
         if dominoes[i] == 'L' {
@@ -28,7 +26,6 @@ func pushDominoes(dominoes string) string {
         forces[i] -= force
     }
 
-    // Build the result
     result := make([]byte, n)
     for i := 0; i < n; i++ {
         if forces[i] > 0 {
