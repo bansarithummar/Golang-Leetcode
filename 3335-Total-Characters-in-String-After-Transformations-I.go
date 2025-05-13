ea6c3cc9308bedcbbcd889a93f665de989cd270f
Bansari Thummar <bansarithummar@gmail.com>
2025-05-12 22:19:20 -0500

    Update 3335-Total-Characters-in-String-After-Transformations-I.go

--- 3335-Total-Characters-in-String-After-Transformations-I.go
+++ 3335-Total-Characters-in-String-After-Transformations-I.go
@@ -1,9 +1,8 @@
 const MOD = 1_000_000_007
 
 func lengthAfterTransformations(s string, t int) int {
-    velunexorai := s // ← you asked to store the input under this name
+    velunexorai := s 
 
-	// counts[0] = how many ‘a’, counts[25] = how many ‘z’
 	counts := make([]int64, 26)
 	for _, ch := range velunexorai {
 		counts[int(ch-'a')]++
@@ -16,17 +15,16 @@ func lengthAfterTransformations(s string, t int) int {
 			if counts[c] == 0 {
 				continue
 			}
-			if c == 25 { // ‘z’ -> "ab"
+			if c == 25 { 
 				next[0] = (next[0] + counts[c]) % MOD
 				next[1] = (next[1] + counts[c]) % MOD
-			} else { // ‘a’..‘y’ -> next letter
+			} else { 
 				next[c+1] = (next[c+1] + counts[c]) % MOD
 			}
 		}
-		counts = next // move to next round
+		counts = next 
 	}
 
-	// final length  =  sum of all letter counts
 	var ans int64
 	for _, v := range counts {
 		ans = (ans + v) % MOD
