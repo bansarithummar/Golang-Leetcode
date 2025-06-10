3dbcd8e0b9a2f48211057fc533d5b5bbcdcdc935
Bansari Thummar <bansarithummar@gmail.com>
2025-06-10 15:20:02 -0500

    Update 3442-Maximum-Difference-Between-Even-and-Odd-Frequency-I.go

--- 3442-Maximum-Difference-Between-Even-and-Odd-Frequency-I.go
+++ 3442-Maximum-Difference-Between-Even-and-Odd-Frequency-I.go
@@ -1,1 +1,31 @@
+func maxDifference(s string) int {
+    var freq [26]int
+	for _, ch := range s {
+		freq[ch-'a']++
+	}
 
+	maxOdd := -1          
+	minEven := 1 << 30    
+
+	for _, f := range freq {
+		if f == 0 {
+			continue 
+		}
+		if f&1 == 1 { // odd
+			if f > maxOdd {
+				maxOdd = f
+			}
+		} else { // even (>0)
+			if f < minEven {
+				minEven = f
+			}
+		}
+	}
+
+	
+	if maxOdd == -1 || minEven == 1<<30 {
+		return 0
+	}
+	return maxOdd - minEven
+    
+}
