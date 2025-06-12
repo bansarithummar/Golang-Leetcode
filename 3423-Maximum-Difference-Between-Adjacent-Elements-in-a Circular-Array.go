7286800e143ab3e8feacf914a4725a506628163a
Bansari Thummar <bansarithummar@gmail.com>
2025-06-11 21:46:29 -0500

    Update 3423-Maximum-Difference-Between-Adjacent-Elements-in-a Circular-Array.go

--- 3423-Maximum-Difference-Between-Adjacent-Elements-in-a Circular-Array.go
+++ 3423-Maximum-Difference-Between-Adjacent-Elements-in-a Circular-Array.go
@@ -1,12 +1,12 @@
 func maxAdjacentDistance(nums []int) int {
     n := len(nums)
-	if n < 2 { // nothing to compare
+	if n < 2 { 
 		return 0
 	}
 
 	maxDiff := 0
 	for i := 0; i < n; i++ {
-		j := (i + 1) % n                    // next index in circular order
+		j := (i + 1) % n                    
 		diff := int(math.Abs(float64(nums[i] - nums[j])))
 		if diff > maxDiff {
 			maxDiff = diff
