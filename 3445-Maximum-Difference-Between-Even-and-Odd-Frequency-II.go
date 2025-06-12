361f13ef704a862f58199319bf1a376e5a31c6c7
Bansari Thummar <bansarithummar@gmail.com>
2025-06-12 17:27:58 -0500

    Update 3445-Maximum-Difference-Between-Even-and-Odd-Frequency-II.go

--- 3445-Maximum-Difference-Between-Even-and-Odd-Frequency-II.go
+++ 3445-Maximum-Difference-Between-Even-and-Odd-Frequency-II.go
@@ -2,17 +2,15 @@ func maxDifference(s string, k int) int {
     n := len(s)
     res := -1000000
     
-    // Try all pairs of characters ('0' to '4')
     for a := '0'; a <= '4'; a++ {
         for b := '0'; b <= '4'; b++ {
             if a == b {
                 continue
             }
             
-            // Transform string: +1 for char a, -1 for char b, 0 for others
             arr := make([]int, n)
-            aa := make([]int, n)  // count of char a
-            bb := make([]int, n)  // count of char b
+            aa := make([]int, n)  
+            bb := make([]int, n)  
             
             for i := 0; i < n; i++ {
                 if rune(s[i]) == a {
@@ -26,25 +24,21 @@ func maxDifference(s string, k int) int {
                 }
             }
             
-            // Build prefix sums
-            p := make([]int, n+1)   // prefix sum of differences
-            pa := make([]int, n+1)  // prefix sum of char a counts
-            pb := make([]int, n+1)  // prefix sum of char b counts
+            p := make([]int, n+1)  
+            pa := make([]int, n+1) 
+            pb := make([]int, n+1)  
             
             for i := 0; i < n; i++ {
                 p[i+1] = p[i] + arr[i]
                 pa[i+1] = pa[i] + aa[i]
                 pb[i+1] = pb[i] + bb[i]
             }
-            
-            // Track minimum prefix values for each parity combination
-            // mn[i][j] = minimum p[x] where pa[x]%2==i and pb[x]%2==j
+
             const INF = 1000000
             mn := [2][2]int{{INF, INF}, {INF, INF}}
             
             i := 0
             for j := 0; j <= n; j++ {
-                // Sliding window: advance left pointer while valid
                 for j-i >= k && pa[j]-pa[i] > 0 && pb[j]-pb[i] > 0 {
                     paIdx := pa[i] % 2
                     pbIdx := pb[i] % 2
@@ -54,8 +48,8 @@ func maxDifference(s string, k int) int {
                     i++
                 }
 
-                targetParityA := (pa[j] + 1) % 2  // opposite of current parity
-                targetParityB := pb[j] % 2        // same as current parity
+                targetParityA := (pa[j] + 1) % 2 
+                targetParityB := pb[j] % 2      
                 
                 if mn[targetParityA][targetParityB] < INF {
                     diff := p[j] - mn[targetParityA][targetParityB]
@@ -65,8 +59,6 @@ func maxDifference(s string, k int) int {
                 }
             }
         }
-    }
-    
-    return res
-    
+    }    
+    return res    
 }
