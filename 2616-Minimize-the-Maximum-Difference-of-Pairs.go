7899af25468a15f7a4ca6c09060cbe99940993ce
Bansari Thummar <bansarithummar@gmail.com>
2025-06-13 11:17:06 -0500

    Update 2616-Minimize-the-Maximum-Difference-of-Pairs.go

--- 2616-Minimize-the-Maximum-Difference-of-Pairs.go
+++ 2616-Minimize-the-Maximum-Difference-of-Pairs.go
@@ -1,1 +1,32 @@
+func minimizeMax(nums []int, p int) int {
+    if p == 0 {
+        return 0
+    }
 
+    sort.Ints(nums)
+    low, high := 0, nums[len(nums)-1]-nums[0]
+
+    canMake := func(maxDiff int) bool {
+        pairs := 0
+        for i := 1; i < len(nums) && pairs < p; {
+            if nums[i]-nums[i-1] <= maxDiff {
+                pairs++
+                i += 2
+            } else {
+                i++
+            }
+        }
+        return pairs >= p
+    }
+
+    for low < high {
+        mid := (low + high) / 2
+        if canMake(mid) {
+            high = mid
+        } else {
+            low = mid + 1
+        }
+    }
+    return low
+    
+}
