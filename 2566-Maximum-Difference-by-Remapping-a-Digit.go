a46ecb6fed2a016b908c8c8e2fd2ad5297bc985d
Bansari Thummar <bansarithummar@gmail.com>
2025-06-14 09:00:27 -0500

    Update 2566-Maximum-Difference-by-Remapping-a-Digit.go

--- 2566-Maximum-Difference-by-Remapping-a-Digit.go
+++ 2566-Maximum-Difference-by-Remapping-a-Digit.go
@@ -1,1 +1,42 @@
+func minMaxDifference(num int) int {
+    s := strconv.Itoa(num)
 
+    // build max value
+    maxRunes := []rune(s)
+    var r rune
+    for _, ch := range maxRunes {
+        if ch != '9' {
+            r = ch
+            break
+        }
+    }
+    if r != 0 {
+        for i, ch := range maxRunes {
+            if ch == r {
+                maxRunes[i] = '9'
+            }
+        }
+    }
+    maxVal, _ := strconv.Atoi(string(maxRunes))
+
+    // build min value
+    minRunes := []rune(s)
+    r = 0
+    for _, ch := range minRunes {
+        if ch != '0' {
+            r = ch
+            break
+        }
+    }
+    if r != 0 {
+        for i, ch := range minRunes {
+            if ch == r {
+                minRunes[i] = '0'
+            }
+        }
+    }
+    minVal, _ := strconv.Atoi(string(minRunes))
+
+    return maxVal - minVal
+    
+}
