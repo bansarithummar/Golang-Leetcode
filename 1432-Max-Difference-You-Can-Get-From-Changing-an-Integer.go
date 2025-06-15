7930706b3fb03894f56dc074c9453b787e56c91d
Bansari Thummar <bansarithummar@gmail.com>
2025-06-15 10:04:41 -0500

    Update 1432-Max-Difference-You-Can-Get-From-Changing-an-Integer.go

--- 1432-Max-Difference-You-Can-Get-From-Changing-an-Integer.go
+++ 1432-Max-Difference-You-Can-Get-From-Changing-an-Integer.go
@@ -1,1 +1,51 @@
+func maxDiff(num int) int {
+    s := strconv.Itoa(num)
 
+    // build max value
+    maxBytes := []byte(s)
+    var tgt byte
+    for _, b := range maxBytes {
+        if b != '9' {
+            tgt = b
+            break
+        }
+    }
+    if tgt != 0 {
+        for i, b := range maxBytes {
+            if b == tgt {
+                maxBytes[i] = '9'
+            }
+        }
+    }
+    maxVal, _ := strconv.Atoi(string(maxBytes))
+
+    // build min value
+    minBytes := []byte(s)
+    if minBytes[0] != '1' {
+        tgt = minBytes[0]
+        for i, b := range minBytes {
+            if b == tgt {
+                minBytes[i] = '1'
+            }
+        }
+    } else {
+        tgt = 0
+        for _, b := range minBytes[1:] {
+            if b != '0' && b != '1' {
+                tgt = b
+                break
+            }
+        }
+        if tgt != 0 {
+            for i, b := range minBytes {
+                if b == tgt {
+                    minBytes[i] = '0'
+                }
+            }
+        }
+    }
+    minVal, _ := strconv.Atoi(string(minBytes))
+
+    return maxVal - minVal
+    
+}
