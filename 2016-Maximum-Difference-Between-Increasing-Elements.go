29077eae72b94a798bbc029be768a0e2dfb61d4c
Bansari Thummar <bansarithummar@gmail.com>
2025-06-16 14:06:02 -0500

    Update 2016-Maximum-Difference-Between-Increasing-Elements.go

--- 2016-Maximum-Difference-Between-Increasing-Elements.go
+++ 2016-Maximum-Difference-Between-Increasing-Elements.go
@@ -1,1 +1,15 @@
-
+func maximumDifference(nums []int) int {
+    minVal := nums[0]
+    maxDiff := -1
+    for _, v := range nums[1:] {
+        if v > minVal {
+            if d := v - minVal; d > maxDiff {
+                maxDiff = d
+            }
+        } else if v < minVal {
+            minVal = v
+        }
+    }
+    return maxDiff
+    
+}
