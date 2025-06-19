0ab097c71f71e3fa0ed88169a5ed5430d2d925e7
Bansari Thummar <bansarithummar@gmail.com>
2025-06-19 16:01:41 -0500

    Update 2294-Partition-Array-Such-That-Maximum-Difference-Is-K.go

--- 2294-Partition-Array-Such-That-Maximum-Difference-Is-K.go
+++ 2294-Partition-Array-Such-That-Maximum-Difference-Is-K.go
@@ -1,1 +1,14 @@
+func partitionArray(nums []int, k int) int {
+    sort.Ints(nums)
+	count := 1
+	start := nums[0]
 
+	for _, v := range nums {
+		if v-start > k {
+			count++
+			start = v
+		}
+	}
+	return count
+    
+}
