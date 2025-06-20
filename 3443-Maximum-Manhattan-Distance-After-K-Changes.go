83b2d006aeeb663a4b40d5a08f800a832dd0f463
Bansari Thummar <bansarithummar@gmail.com>
2025-06-20 10:05:41 -0500

    Update 3443-Maximum-Manhattan-Distance-After-K-Changes.go

--- 3443-Maximum-Manhattan-Distance-After-K-Changes.go
+++ 3443-Maximum-Manhattan-Distance-After-K-Changes.go
@@ -1,1 +1,40 @@
+func maxDistance(s string, k int) int {
+    abs := func(a int) int {
+		if a < 0 {
+			return -a
+		}
+		return a
+	}
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
 
+	ans := 0
+	north, south, east, west := 0, 0, 0, 0
+
+	for i, c := range s {
+		switch c {
+		case 'N':
+			north++
+		case 'S':
+			south++
+		case 'E':
+			east++
+		case 'W':
+			west++
+		}
+
+		x := abs(north - south)
+		y := abs(east - west)
+		md := x + y                                 
+		extra := min(2*k, (i+1)-md)                 
+		if dist := md + extra; dist > ans {
+			ans = dist
+		}
+	}
+	return ans
+    
+}
