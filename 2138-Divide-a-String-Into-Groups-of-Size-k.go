c368717f6bd6f14df7d006a15f11647c753bbfff
Bansari Thummar <bansarithummar@gmail.com>
2025-06-22 09:24:08 -0500

    Update 2138-Divide-a-String-Into-Groups-of-Size-k.go

--- 2138-Divide-a-String-Into-Groups-of-Size-k.go
+++ 2138-Divide-a-String-Into-Groups-of-Size-k.go
@@ -1,1 +1,12 @@
-
+func divideString(s string, k int, fill byte) []string {
+    rem := len(s) % k
+	if rem != 0 {
+		s += strings.Repeat(string(fill), k-rem)
+	}
+	res := make([]string, 0, len(s)/k)
+	for i := 0; i < len(s); i += k {
+		res = append(res, s[i:i+k])
+	}
+	return res
+    
+}
