64231b06ce59244e52c3ea98f78f53f8633d2158
Bansari Thummar <bansarithummar@gmail.com>
2025-06-26 08:49:12 -0500

    Update 2311-Longest-Binary-Subsequence-Less-Than-or-Equal-to-K.go

--- 2311-Longest-Binary-Subsequence-Less-Than-or-Equal-to-K.go
+++ 2311-Longest-Binary-Subsequence-Less-Than-or-Equal-to-K.go
@@ -1,1 +1,22 @@
+func abs64(x int64) int64 { if x < 0 { return -x }; return x }
 
+func longestSubsequence(s string, k int) int {
+    zeros := 0
+	for _, c := range s {
+		if c == '0' {
+			zeros++
+		}
+	}
+
+	val, pow := int64(0), int64(1) 
+	ones := 0
+	for i := len(s) - 1; i >= 0 && pow <= int64(k); i-- {
+		if s[i] == '1' && val+pow <= int64(k) {
+			val += pow
+			ones++
+		}
+		pow <<= 1
+	}
+
+	return zeros + ones  
+}
