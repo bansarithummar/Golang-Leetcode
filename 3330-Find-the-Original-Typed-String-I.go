d0cc64b9c3acf310c8475a1c3c05b9ee3ee4d29e
Bansari Thummar <bansarithummar@gmail.com>
2025-07-01 11:24:51 -0500

    Update 3330-Find-the-Original-Typed-String-I.go

--- 3330-Find-the-Original-Typed-String-I.go
+++ 3330-Find-the-Original-Typed-String-I.go
@@ -1,1 +1,26 @@
+func possibleStringCount(word string) int {
+    type group struct {
+		ch    byte
+		count int
+	}
+	groups := make([]group, 0)
+	n := len(word)
+	i := 0
 
+	for i < n {
+		j := i
+		for j < n && word[j] == word[i] {
+			j++
+		}
+		groups = append(groups, group{ch: word[i], count: j - i})
+		i = j
+	}
+
+	total := 1
+	for _, g := range groups {
+		if g.count > 1 {
+			total += g.count - 1
+		}
+	}
+	return total    
+}
