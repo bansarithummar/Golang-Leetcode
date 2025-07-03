adc27f1980721c6cab2248ad17ed1d9e9f61a5eb
Bansari Thummar <bansarithummar@gmail.com>
2025-07-03 10:37:17 -0500

    Update 3304-Find-the-K-th-Character-in-String-Game-I.go

--- 3304-Find-the-K-th-Character-in-String-Game-I.go
+++ 3304-Find-the-K-th-Character-in-String-Game-I.go
@@ -1,1 +1,15 @@
-
+func kthCharacter(k int) byte {
+    word := []byte{'a'}
+	for len(word) < k {
+		next := make([]byte, len(word))
+		for i := 0; i < len(word); i++ {
+			if word[i] == 'z' {
+				next[i] = 'a'
+			} else {
+				next[i] = word[i] + 1
+			}
+		}
+		word = append(word, next...)
+	}
+	return word[k-1]
+}
