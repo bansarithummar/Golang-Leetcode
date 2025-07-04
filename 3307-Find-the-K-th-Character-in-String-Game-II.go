ff66a11364bfa04447e263121cc66419c818acc0
Bansari Thummar <bansarithummar@gmail.com>
2025-07-04 11:21:58 -0500

    Update 3307-Find-the-K-th-Character-in-String-Game-II.go

--- 3307-Find-the-K-th-Character-in-String-Game-II.go
+++ 3307-Find-the-K-th-Character-in-String-Game-II.go
@@ -1,1 +1,16 @@
-
+func kthCharacter(k int64, operations []int) byte {
+    var shifts int64
+    for k != 1 {
+        i := 63 - bits.LeadingZeros64(uint64(k))
+        half := int64(1) << i
+        if half == k {
+            i--
+            half = int64(1) << i
+        }
+        k -= half
+        if operations[i] == 1 {
+            shifts++
+        }
+    }
+    return byte(shifts%26 + 97)  
+}
