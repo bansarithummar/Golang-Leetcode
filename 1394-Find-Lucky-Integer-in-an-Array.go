268c76d97519622fde817a6c6e79bfeda2552074
Bansari Thummar <bansarithummar@gmail.com>
2025-07-05 08:30:37 -0500

    Update 1394-Find-Lucky-Integer-in-an-Array.go

--- 1394-Find-Lucky-Integer-in-an-Array.go
+++ 1394-Find-Lucky-Integer-in-an-Array.go
@@ -1,1 +1,16 @@
+func findLucky(arr []int) int {
+    freqMap := make(map[int]int)
+    
+    for _, num := range arr {
+        freqMap[num]++
+    }
 
+    res := -1
+    for num, freq := range freqMap {
+        if num == freq && num > res {
+            res = num
+        }
+    }
+
+    return res   
+}
