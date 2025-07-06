7aa28756362994f2a45fa4b6ae975ee09ba8498c
Bansari Thummar <bansarithummar@gmail.com>
2025-07-06 09:20:28 -0500

    Update 1865-Finding-Pairs-With-a-Certain-Sum.go

--- 1865-Finding-Pairs-With-a-Certain-Sum.go
+++ 1865-Finding-Pairs-With-a-Certain-Sum.go
@@ -1,1 +1,39 @@
+type FindSumPairs struct {
+    nums1 []int
+    nums2 []int
+    freq2 map[int]int
+    
+}
 
+
+func Constructor(nums1 []int, nums2 []int) FindSumPairs {
+    freq := make(map[int]int)
+    for _, num := range nums2 {
+        freq[num]++
+    }
+    return FindSumPairs{
+        nums1: nums1,
+        nums2: nums2,
+        freq2: freq,
+    }
+    
+}
+
+
+func (this *FindSumPairs) Add(index int, val int)  {
+    oldVal := this.nums2[index]
+    this.freq2[oldVal]--
+    this.nums2[index] += val
+    this.freq2[this.nums2[index]]++
+    
+}
+
+
+func (this *FindSumPairs) Count(tot int) int {
+    count := 0
+    for _, num := range this.nums1 {
+        count += this.freq2[tot - num]
+    }
+    return count
+    
+}
