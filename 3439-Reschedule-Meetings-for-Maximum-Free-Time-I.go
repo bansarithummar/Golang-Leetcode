68647af37ff81912e4f8fddd0a24a2f147474e88
Bansari Thummar <bansarithummar@gmail.com>
2025-07-09 09:37:25 -0500

    Update 3439-Reschedule-Meetings-for-Maximum-Free-Time-I.go

--- 3439-Reschedule-Meetings-for-Maximum-Free-Time-I.go
+++ 3439-Reschedule-Meetings-for-Maximum-Free-Time-I.go
@@ -1,1 +1,31 @@
+func maxFreeTime(eventTime int, k int, startTime []int, endTime []int) int {
+    n := len(startTime)
+	res := 0
+	sumDurations := make([]int, n+1)
 
+	for i := 0; i < n; i++ {
+		sumDurations[i+1] = sumDurations[i] + endTime[i] - startTime[i]
+	}
+
+	for i := k - 1; i < n; i++ {
+		right := 0
+		if i == n-1 {
+			right = eventTime
+		} else {
+			right = startTime[i+1]
+		}
+
+		left := 0
+		if i != k-1 {
+			left = endTime[i-k]
+		}
+
+		freeTime := right - left - (sumDurations[i+1] - sumDurations[i-k+1])
+		if freeTime > res {
+			res = freeTime
+		}
+	}
+
+	return res
+    
+}
