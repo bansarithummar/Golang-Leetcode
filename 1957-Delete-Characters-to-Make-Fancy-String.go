6a9d2d27e5864115d9926bf27cf27a0b2ae247d1
Bansari Thummar <bansarithummar@gmail.com>
2025-07-21 09:25:38 -0500

    Update 1957-Delete-Characters-to-Make-Fancy-String.go

--- 1957-Delete-Characters-to-Make-Fancy-String.go
+++ 1957-Delete-Characters-to-Make-Fancy-String.go
@@ -1,1 +1,19 @@
+func makeFancyString(s string) string {
+    var res []byte
+    count := 1
 
+    for i := 0; i < len(s); i++ {
+        if i > 0 && s[i] == s[i-1] {
+            count++
+        } else {
+            count = 1
+        }
+
+        if count < 3 {
+            res = append(res, s[i])
+        }
+    }
+
+    return string(res)
+    
+}
