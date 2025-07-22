6cac6d73e61370dcb37705f8891f5a0ef9c4499c
Bansari Thummar <bansarithummar@gmail.com>
2025-07-22 08:46:43 -0500

    Update 1695-Maximum-Erasure-Value.go

--- 1695-Maximum-Erasure-Value.go
+++ 1695-Maximum-Erasure-Value.go
@@ -1,1 +1,20 @@
+func maximumUniqueSubarray(nums []int) int {
+    seen := make(map[int]bool)
+    left, sum, maxSum := 0, 0, 0
 
+    for right := 0; right < len(nums); right++ {
+        for seen[nums[right]] {
+            seen[nums[left]] = false
+            sum -= nums[left]
+            left++
+        }
+        seen[nums[right]] = true
+        sum += nums[right]
+        if sum > maxSum {
+            maxSum = sum
+        }
+    }
+
+    return maxSum
+    
+}
