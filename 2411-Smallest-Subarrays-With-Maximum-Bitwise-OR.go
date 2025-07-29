b0b6847d022227ca194e32d5b744a13cb5fe1b23
Bansari Thummar <bansarithummar@gmail.com>
2025-07-29 14:53:08 -0700

    Update 2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.go

--- 2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.go
+++ 2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.go
@@ -1,1 +1,28 @@
+func smallestSubarrays(nums []int) []int {
+    n := len(nums)
+    answer := make([]int, n)
+    lastSeen := make([]int, 32) // last index for each bit
 
+    for i := 0; i < 32; i++ {
+        lastSeen[i] = -1
+    }
+
+    for i := n - 1; i >= 0; i-- {
+        for bit := 0; bit < 32; bit++ {
+            if (nums[i]>>bit)&1 == 1 {
+                lastSeen[bit] = i
+            }
+        }
+
+        maxIdx := i
+        for bit := 0; bit < 32; bit++ {
+            if lastSeen[bit] > maxIdx {
+                maxIdx = lastSeen[bit]
+            }
+        }
+
+        answer[i] = maxIdx - i + 1
+    }
+
+    return answer   
+}
