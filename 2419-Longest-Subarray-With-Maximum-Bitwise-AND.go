5096b97018d97299ef3ccf8c0af9678a17800295
Bansari Thummar <bansarithummar@gmail.com>
2025-07-30 10:31:23 -0700

    Update 2419-Longest-Subarray-With-Maximum-Bitwise-AND.go

--- 2419-Longest-Subarray-With-Maximum-Bitwise-AND.go
+++ 2419-Longest-Subarray-With-Maximum-Bitwise-AND.go
@@ -1,1 +1,24 @@
+func longestSubarray(nums []int) int {
+    maxVal := 0
+    for _, num := range nums {
+        if num > maxVal {
+            maxVal = num
+        }
+    }
 
+    longest := 0
+    count := 0
+
+    for _, num := range nums {
+        if num == maxVal {
+            count++
+            if count > longest {
+                longest = count
+            }
+        } else {
+            count = 0
+        }
+    }
+
+    return longest   
+}
