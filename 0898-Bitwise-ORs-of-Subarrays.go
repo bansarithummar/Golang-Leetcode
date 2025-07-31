68c6fb1a914abf2a4e72c45eb857ba8903adf8cc
Bansari Thummar <bansarithummar@gmail.com>
2025-07-31 14:39:23 -0500

    Update 0898-Bitwise-ORs-of-Subarrays.go

--- 0898-Bitwise-ORs-of-Subarrays.go
+++ 0898-Bitwise-ORs-of-Subarrays.go
@@ -1,1 +1,19 @@
+func subarrayBitwiseORs(arr []int) int {
+    prevSet := map[int]bool{}
+    resSet := map[int]bool{}
 
+    for _, num := range arr {
+        currSet := map[int]bool{}
+        currSet[num] = true
+        for val := range prevSet {
+            currSet[val|num] = true
+        }
+        for val := range currSet {
+            resSet[val] = true
+        }
+        prevSet = currSet
+    }
+
+    return len(resSet)
+    
+}
