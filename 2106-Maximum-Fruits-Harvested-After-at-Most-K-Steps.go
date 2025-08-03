8691a6b21f993580c02034b2f9126db7af0d5f97
Bansari Thummar <bansarithummar@gmail.com>
2025-08-03 12:04:11 -0500

    Update 2106-Maximum-Fruits-Harvested-After-at-Most-K-Steps.go

--- 2106-Maximum-Fruits-Harvested-After-at-Most-K-Steps.go
+++ 2106-Maximum-Fruits-Harvested-After-at-Most-K-Steps.go
@@ -1,1 +1,44 @@
+func maxTotalFruits(fruits [][]int, startPos int, k int) int {
+    n := len(fruits)
+    prefixSum := make([]int, n+1)
+    for i := 0; i < n; i++ {
+        prefixSum[i+1] = prefixSum[i] + fruits[i][1]
+    }
 
+    maxFruits := 0
+
+    left := 0
+    for right := 0; right < n; right++ {
+        for left <= right {
+            l := fruits[left][0]
+            r := fruits[right][0]
+            dist := min(abs(startPos-l)+r-l, abs(startPos-r)+r-l)
+            if dist <= k {
+                break
+            }
+            left++
+        }
+        if left <= right {
+            sum := prefixSum[right+1] - prefixSum[left]
+            if sum > maxFruits {
+                maxFruits = sum
+            }
+        }
+    }
+
+    return maxFruits
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b    
+}
