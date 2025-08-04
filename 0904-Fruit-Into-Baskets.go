011ea82fe754f69fda4101dd6f66b9e3b29f9b13
Bansari Thummar <bansarithummar@gmail.com>
2025-08-04 09:29:02 -0500

    Update 0904-Fruit-Into-Baskets.go

--- 0904-Fruit-Into-Baskets.go
+++ 0904-Fruit-Into-Baskets.go
@@ -1,1 +1,22 @@
+func totalFruit(fruits []int) int {
+    basket := make(map[int]int)
+    left := 0
+    maxFruits := 0
 
+    for right, fruit := range fruits {
+        basket[fruit]++
+
+        for len(basket) > 2 {
+            basket[fruits[left]]--
+            if basket[fruits[left]] == 0 {
+                delete(basket, fruits[left])
+            }
+            left++
+        }
+
+        if right-left+1 > maxFruits {
+            maxFruits = right - left + 1
+        }
+    }
+    return maxFruits  
+}
