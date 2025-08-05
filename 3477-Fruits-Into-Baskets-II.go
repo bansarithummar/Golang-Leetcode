481ef39b3f9e40e827c9e911928ae421dbb54cf2
Bansari Thummar <bansarithummar@gmail.com>
2025-08-05 10:29:50 -0500

    Update 3477-Fruits-Into-Baskets-II.go

--- 3477-Fruits-Into-Baskets-II.go
+++ 3477-Fruits-Into-Baskets-II.go
@@ -1,1 +1,21 @@
+func numOfUnplacedFruits(fruits []int, baskets []int) int {
+    used := make([]bool, len(baskets))
+	unplaced := 0
 
+	for _, fruit := range fruits {
+		placed := false
+		for j := 0; j < len(baskets); j++ {
+			if !used[j] && baskets[j] >= fruit {
+				used[j] = true
+				placed = true
+				break
+			}
+		}
+		if !placed {
+			unplaced++
+		}
+	}
+
+	return unplaced
+    
+}
