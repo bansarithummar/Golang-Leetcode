2b32df4a0cd4963b5bb378c0b2b1afb819d53d02
Bansari Thummar <bansarithummar@gmail.com>
2025-08-10 10:39:16 -0500

    Update 0869-Reordered-Power-of-2.go

--- 0869-Reordered-Power-of-2.go
+++ 0869-Reordered-Power-of-2.go
@@ -1,1 +1,26 @@
+func reorderedPowerOf2(n int) bool {
+    target := countDigits(n)
+    for i := 0; i < 31; i++ {
+        if equalCounts(target, countDigits(1<<i)) {
+            return true
+        }
+    }
+    return false
+}
 
+func countDigits(num int) [10]int {
+    var cnt [10]int
+    for _, ch := range strconv.Itoa(num) {
+        cnt[ch-'0']++
+    }
+    return cnt
+}
+
+func equalCounts(a, b [10]int) bool {
+    for i := 0; i < 10; i++ {
+        if a[i] != b[i] {
+            return false
+        }
+    }
+    return true   
+}
