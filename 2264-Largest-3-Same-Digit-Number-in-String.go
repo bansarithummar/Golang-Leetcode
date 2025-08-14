76e7decfbe7d28bdf10a31a36bec78dc80b5a74e
Bansari Thummar <bansarithummar@gmail.com>
2025-08-14 11:24:22 -0500

    Update 2264-Largest-3-Same-Digit-Number-in-String.go

--- 2264-Largest-3-Same-Digit-Number-in-String.go
+++ 2264-Largest-3-Same-Digit-Number-in-String.go
@@ -1,1 +1,13 @@
-
+func largestGoodInteger(num string) string {
+    res := ""
+    for i := 0; i <= len(num)-3; i++ {
+        if num[i] == num[i+1] && num[i+1] == num[i+2] {
+            cur := num[i : i+3]
+            if cur > res {
+                res = cur
+            }
+        }
+    }
+    return res
+    
+}
