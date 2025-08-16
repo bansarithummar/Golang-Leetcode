9ebe1478ad400b1e84ee516c040f03e5ffd6a1a7
Bansari Thummar <bansarithummar@gmail.com>
2025-08-16 14:33:03 -0500

    Update 1323-Maximum-69-Number.go

--- 1323-Maximum-69-Number.go
+++ 1323-Maximum-69-Number.go
@@ -1,1 +1,11 @@
-
+func maximum69Number (num int) int {
+    digits := []byte(strconv.Itoa(num))
+    for i := 0; i < len(digits); i++ {
+        if digits[i] == '6' {
+            digits[i] = '9'
+            break
+        }
+    }
+    ans, _ := strconv.Atoi(string(digits))
+    return ans   
+}
