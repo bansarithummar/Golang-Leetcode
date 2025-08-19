91b859b212cc48c4d5958a59a728c1c2a6536cf2
Bansari Thummar <bansarithummar@gmail.com>
2025-08-19 10:09:19 -0500

    Update 2348-Number-of-Zero-Filled-Subarrays.go

--- 2348-Number-of-Zero-Filled-Subarrays.go
+++ 2348-Number-of-Zero-Filled-Subarrays.go
@@ -1,1 +1,14 @@
+func zeroFilledSubarray(nums []int) int64 {
+    var res int64 = 0
+	var streak int64 = 0
 
+	for _, v := range nums {
+		if v == 0 {
+			streak++
+			res += streak
+		} else {
+			streak = 0
+		}
+	}
+	return res  
+}
