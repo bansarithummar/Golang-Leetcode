3711968097ecf726b159a418d4aa08132487ae2f
Bansari Thummar <bansarithummar@gmail.com>
2025-08-20 12:03:37 -0500

    Update 1277-Count-Square-Submatrices-with-All-Ones.go

--- 1277-Count-Square-Submatrices-with-All-Ones.go
+++ 1277-Count-Square-Submatrices-with-All-Ones.go
@@ -5,20 +5,17 @@ func countSquares(matrix [][]int) int {
 	}
 	n := len(matrix[0])
 
-	// dp[j] will store the size of the largest all-ones square
-	// ending at current row i and column j.
 	dp := make([]int, n)
 	ans := 0
 
 	for i := 0; i < m; i++ {
-		prevDiag := 0 // this will hold dp[j-1] from the previous row (top-left)
+		prevDiag := 0 
 		for j := 0; j < n; j++ {
-			temp := dp[j] // save current dp[j] (which is top for next iteration)
+			temp := dp[j] 
 			if matrix[i][j] == 1 {
 				if i == 0 || j == 0 {
 					dp[j] = 1
 				} else {
-					// min of (top, left, top-left) + 1
 					minNeighbor := dp[j]
 					if dp[j-1] < minNeighbor {
 						minNeighbor = dp[j-1]
