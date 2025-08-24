396612cc98ed841e77ace1505708600b850590f7
Bansari Thummar <bansarithummar@gmail.com>
2025-08-24 09:21:29 -0500

    Update 1493-Longest-Subarray-of-1's-After-Deleting-One-Element.go

--- 1493-Longest-Subarray-of-1's-After-Deleting-One-Element.go
+++ 1493-Longest-Subarray-of-1's-After-Deleting-One-Element.go
@@ -1,1 +1,19 @@
-
+func longestSubarray(nums []int) int {
+    l, zeros, best := 0, 0, 0
+	for r := 0; r < len(nums); r++ {
+		if nums[r] == 0 {
+			zeros++
+		}
+		for zeros > 1 {
+			if nums[l] == 0 {
+				zeros--
+			}
+			l++
+		}
+		if r-l > best { // delete one element in window
+			best = r - l
+		}
+	}
+	return best
+    
+}
