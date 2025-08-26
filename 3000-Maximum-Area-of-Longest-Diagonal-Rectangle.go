5aac078701a8d68aed55ab0247c98783da77d618
Bansari Thummar <bansarithummar@gmail.com>
2025-08-26 11:21:41 -0700

    Update 3000-Maximum-Area-of-Longest-Diagonal-Rectangle.go

--- 3000-Maximum-Area-of-Longest-Diagonal-Rectangle.go
+++ 3000-Maximum-Area-of-Longest-Diagonal-Rectangle.go
@@ -1,1 +1,12 @@
-
+func areaOfMaxDiagonal(dimensions [][]int) int {
+    bestD2, bestArea := -1, 0
+	for _, d := range dimensions {
+		l, w := d[0], d[1]
+		d2 := l*l + w*w
+		area := l * w
+		if d2 > bestD2 || (d2 == bestD2 && area > bestArea) {
+			bestD2, bestArea = d2, area
+		}
+	}
+	return bestArea
+}
