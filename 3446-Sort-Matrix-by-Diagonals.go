82c4eb92a3e3855b7c9bd091c0041f935ab411d7
Bansari Thummar <bansarithummar@gmail.com>
2025-08-28 10:49:33 -0700

    Update 3446-Sort-Matrix-by-Diagonals.go

--- 3446-Sort-Matrix-by-Diagonals.go
+++ 3446-Sort-Matrix-by-Diagonals.go
@@ -1,1 +1,30 @@
+func sortMatrix(grid [][]int) [][]int {
+    n := len(grid)
+	diag := make(map[int][]int) 
 
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			k := i - j
+			diag[k] = append(diag[k], grid[i][j])
+		}
+	}
+
+	for k, a := range diag {
+		if k >= 0 {
+			sort.Slice(a, func(i, j int) bool { return a[i] > a[j] })
+		} else {
+			sort.Ints(a)
+		}
+		diag[k] = a
+	}
+
+	pos := make(map[int]int)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			k := i - j
+			grid[i][j] = diag[k][pos[k]]
+			pos[k]++
+		}
+	}
+	return grid    
+}
