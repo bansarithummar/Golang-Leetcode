84a672323bd06edd17314e4848d3f5d1e6af27b3
Bansari Thummar <bansarithummar@gmail.com>
2025-09-05 14:23:05 -0700

    Update 2749-Minimum-Operations-to-Make-the-Integer-Zero.go

--- 2749-Minimum-Operations-to-Make-the-Integer-Zero.go
+++ 2749-Minimum-Operations-to-Make-the-Integer-Zero.go
@@ -1,1 +1,12 @@
-
+func makeTheIntegerZero(num1 int, num2 int) int {
+    for k := 1; k <= 60; k++ {
+		x := int64(num1) - int64(k)*int64(num2)
+		if x < int64(k) || x < 0 {
+			continue
+		}
+		if bits.OnesCount64(uint64(x)) <= k {
+			return k
+		}
+	}
+	return -1  
+}
