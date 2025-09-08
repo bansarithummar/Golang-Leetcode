185555767c1451041fb9f61d1de82141d0b56e5e
Bansari Thummar <bansarithummar@gmail.com>
2025-09-08 15:20:16 -0700

    Update 1317-Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.go

--- 1317-Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.go
+++ 1317-Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.go
@@ -1,1 +1,20 @@
+func getNoZeroIntegers(n int) []int {
+    isValid := func(x int) bool {
+		for x > 0 {
+			if x%10 == 0 {
+				return false
+			}
+			x /= 10
+		}
+		return true
+	}
 
+	for a := 1; a < n; a++ {
+		b := n - a
+		if isValid(a) && isValid(b) {
+			return []int{a, b}
+		}
+	}
+	return nil
+    
+}
