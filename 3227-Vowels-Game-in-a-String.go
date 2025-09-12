e89d77c69f8267519e0f4bddf52e4e253ad40771
Bansari Thummar <bansarithummar@gmail.com>
2025-09-12 10:33:22 -0700

    Update 3227-Vowels-Game-in-a-String.go

--- 3227-Vowels-Game-in-a-String.go
+++ 3227-Vowels-Game-in-a-String.go
@@ -1,1 +1,14 @@
-
+func doesAliceWin(s string) bool {
+    vowels := map[byte]bool{
+        'a': true, 'e': true, 'i': true, 'o': true, 'u': true,
+    }
+    count := 0
+    for i := 0; i < len(s); i++ {
+        if vowels[s[i]] {
+            count++
+        }
+    }
+    // Alice wins if there is at least one vowel (odd substring exists)
+    return count > 0
+    
+}
