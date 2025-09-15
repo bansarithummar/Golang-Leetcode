52a84b8911dbc848d2d7f5783cecacec84e7239e
Bansari Thummar <bansarithummar@gmail.com>
2025-09-15 10:15:51 -0700

    Update 1935-Maximum-Number-of-Words-You-Can-Type.go

--- 1935-Maximum-Number-of-Words-You-Can-Type.go
+++ 1935-Maximum-Number-of-Words-You-Can-Type.go
@@ -1,1 +1,21 @@
-
+func canBeTypedWords(text string, brokenLetters string) int {
+    broken := make(map[rune]bool)
+    for _, ch := range brokenLetters {
+        broken[ch] = true
+    }
+    count := 0
+    words := strings.Split(text, " ")
+    for _, w := range words {
+        ok := true
+        for _, ch := range w {
+            if broken[ch] {
+                ok = false
+                break
+            }
+        }
+        if ok {
+            count++
+        }
+    }
+    return count   
+}
