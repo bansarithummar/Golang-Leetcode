5112b523cd383b96896fd9c3f7da049c3b923c72
Bansari Thummar <bansarithummar@gmail.com>
2025-09-16 10:53:24 -0700

    Update 2197-Replace-Non-Coprime-Numbers-in-Array.go

--- 2197-Replace-Non-Coprime-Numbers-in-Array.go
+++ 2197-Replace-Non-Coprime-Numbers-in-Array.go
@@ -1,1 +1,27 @@
+func replaceNonCoprimes(nums []int) []int {
+    gcd := func(a, b int) int {
+        for b != 0 {
+            a, b = b, a%b
+        }
+        return a
+    }
+    lcm := func(a, b int) int {
+        return a / gcd(a, b) * b
+    }
 
+    st := []int{}
+    for _, x := range nums {
+        st = append(st, x)
+        for len(st) > 1 {
+            top := st[len(st)-1]
+            prev := st[len(st)-2]
+            g := gcd(top, prev)
+            if g == 1 {
+                break
+            }
+            st = st[:len(st)-2]
+            st = append(st, lcm(top, prev))
+        }
+    }
+    return st   
+}
