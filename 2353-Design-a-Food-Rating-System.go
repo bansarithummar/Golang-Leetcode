6c52edf40082e958d777d373dda0af1382241111
Bansari Thummar <bansarithummar@gmail.com>
2025-09-17 10:15:45 -0700

    Update 2353-Design-a-Food-Rating-System.go

--- 2353-Design-a-Food-Rating-System.go
+++ 2353-Design-a-Food-Rating-System.go
@@ -3,15 +3,14 @@ type Item struct {
 	rating int
 }
 
-// FoodHeap is a max-heap ordered by rating desc, then name asc.
 type FoodHeap []Item
 
 func (h FoodHeap) Len() int { return len(h) }
 func (h FoodHeap) Less(i, j int) bool {
 	if h[i].rating == h[j].rating {
-		return h[i].name < h[j].name // lexicographically smaller first
+		return h[i].name < h[j].name 
 	}
-	return h[i].rating > h[j].rating // larger rating first
+	return h[i].rating > h[j].rating 
 }
 func (h FoodHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
 func (h *FoodHeap) Push(x interface{}) {
@@ -60,7 +59,6 @@ func (this *FoodRatings) ChangeRating(food string, newRating int) {
 		this.cuisineHeap[c] = h
 		heap.Init(h)
 	}
-	// push a new snapshot; lazy removal of stale snapshots will occur in HighestRated
 	heap.Push(this.cuisineHeap[c], Item{name: food, rating: newRating})
 }
 
@@ -69,14 +67,13 @@ func (this *FoodRatings) HighestRated(cuisine string) string {
 	if !ok || h.Len() == 0 {
 		return ""
 	}
-	// Pop stale entries until top matches current rating
 	for h.Len() > 0 {
 		top := (*h)[0]
 		curr := this.foodToRating[top.name]
 		if top.rating == curr {
 			return top.name
 		}
-		heap.Pop(h) // discard stale snapshot
+		heap.Pop(h) 
 	}
 	return ""
 }
