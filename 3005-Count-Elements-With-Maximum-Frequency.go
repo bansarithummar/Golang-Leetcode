888f579da1a1b19c7ad524f25dc28e7e263c72cc
Bansari Thummar <bansarithummar@gmail.com>
2025-09-22 10:49:07 -0700

    Update 3005-Count-Elements-With-Maximum-Frequency.go

--- 3005-Count-Elements-With-Maximum-Frequency.go
+++ 3005-Count-Elements-With-Maximum-Frequency.go
@@ -1,1 +1,17 @@
-
+func maxFrequencyElements(nums []int) int {
+    freq := make(map[int]int)
+    maxFreq := 0
+    for _, v := range nums {
+        freq[v]++
+        if freq[v] > maxFreq {
+            maxFreq = freq[v]
+        }
+    }
+    res := 0
+    for _, f := range freq {
+        if f == maxFreq {
+            res += f
+        }
+    }
+    return res
+}
